grnet/synnefo

commit 5c812d1b2a7aa60658893f43721585207e890007

Split some loosely related modules

api/resource.py
@@ -1,0 +1,37 @@
+# vim: ts=4 sts=4 et ai sw=4 fileencoding=utf-8
+#
+# Copyright © 2010 Greek Research and Technology Network
+#
+
+from piston.resource import Resource as BaseResource
+from synnefo.helpers import parse_accept_header
+
+class Resource(BaseResource):
+    def determine_emitter(self, request, *args, **kwargs):
+        """
+        Override default emitter policy to account for Accept header
+
+        emitter_format (.json or .xml suffix in URL) always takes precedence.
+
+        After that, the Accept header is checked; if both JSON and XML are
+        equally preferred, use JSON.
+
+        If none of the two were provided, then use JSON as per the
+        specification.
+        """
+
+        em = request.GET.get('format', 'json')
+        if 'emitter_format' in kwargs and \
+           kwargs["emitter_format"] is not None:
+            em = kwargs.pop('emitter_format')
+        elif 'HTTP_ACCEPT' in request.META:
+            accepts = parse_accept_header(request.META['HTTP_ACCEPT'])
+            for content_type, quality in accepts:
+                if content_type == 'application/json':
+                    break
+                elif content_type == 'application/xml':
+                    em = request.GET.get('format', 'xml')
+                    break
+
+        return em
+

api/emitter.py
@@ -3,64 +3,8 @@
 # Copyright © 2010 Greek Research and Technology Network
 #
 
-import re
 from xml.dom import minidom
-from django.conf.urls.defaults import url
 from piston.emitters import Emitter, Mimer
-from piston.resource import Resource as BaseResource
-
-_accept_re = re.compile(r'([^\s;,]+)(?:[^,]*?;\s*q=(\d*(?:\.\d+)?))?')
-
-def parse_accept_header(value):
-    """Parse an HTTP Accept header
-
-    Returns an ordered by quality list of tuples (value, quality)
-    """
-    if not value:
-        return []
-
-    result = []
-    for match in _accept_re.finditer(value):
-        quality = match.group(2)
-        if not quality:
-            quality = 1
-        else:
-            quality = max(min(float(quality), 1), 0)
-        result.append((match.group(1), quality))
-
-    # sort by quality
-    result.sort(key=lambda x: x[1])
-
-    return result
-
-class Resource(BaseResource):
-    def determine_emitter(self, request, *args, **kwargs):
-        """
-        Override default emitter policy to account for Accept header
-
-        emitter_format (.json or .xml suffix in URL) always takes precedence.
-
-        After that, the Accept header is checked; if both JSON and XML are
-        equally preferred, use JSON.
-
-        If none of the two were provided, then use JSON as per the
-        specification.
-        """
-
-        em = request.GET.get('format', 'json')
-        if 'emitter_format' in kwargs and \
-           kwargs["emitter_format"] is not None:
-            em = kwargs.pop('emitter_format')
-        elif 'HTTP_ACCEPT' in request.META:
-            accepts = parse_accept_header(request.META['HTTP_ACCEPT'])
-            for content_type, quality in accepts:
-                if content_type == 'application/json':
-                    break
-                elif content_type == 'application/xml':
-                    em = request.GET.get('format', 'xml')
-                    break
-
-        return em
 
 class OSXMLEmitter(Emitter):
     """
@@ -118,12 +62,3 @@ class OSXMLEmitter(Emitter):
 
 Emitter.register('xml', OSXMLEmitter, 'application/xml')
 Mimer.register(lambda *a: None, ('application/xml',))
-
-def url_with_format(regex, *args, **kwargs):
-    """
-    An extended url() that adds an .json/.xml suffix to the end to avoid DRY
-    """
-    if regex[-1] == '$' and regex[-2] != '\\':
-        regex = regex[:-1]
-    regex = regex + r'(\.(?P<emitter_format>json|xml))?$'
-    return url(regex, *args, **kwargs)

api/urls.py
@@ -4,7 +4,8 @@
 #
 
 from django.conf.urls.defaults import *
-from synnefo.api.emitter import Resource, url_with_format
+from synnefo.helpers import url_with_format
+from synnefo.api.resource import Resource
 from synnefo.api.handlers import *
 from synnefo.api.authentication import TokenAuthentication
 from synnefo.api.faults import fault

helpers.py
@@ -1,0 +1,40 @@
+# vim: ts=4 sts=4 et ai sw=4 fileencoding=utf-8
+#
+# Copyright © 2010 Greek Research and Technology Network
+#
+
+import re
+from django.conf.urls.defaults import url
+
+_accept_re = re.compile(r'([^\s;,]+)(?:[^,]*?;\s*q=(\d*(?:\.\d+)?))?')
+
+def parse_accept_header(value):
+    """Parse an HTTP Accept header
+
+    Returns an ordered by quality list of tuples (value, quality)
+    """
+    if not value:
+        return []
+
+    result = []
+    for match in _accept_re.finditer(value):
+        quality = match.group(2)
+        if not quality:
+            quality = 1
+        else:
+            quality = max(min(float(quality), 1), 0)
+        result.append((match.group(1), quality))
+
+    # sort by quality
+    result.sort(key=lambda x: x[1])
+
+    return result
+
+def url_with_format(regex, *args, **kwargs):
+    """
+    An extended url() that adds an .json/.xml suffix to the end to avoid DRY
+    """
+    if regex[-1] == '$' and regex[-2] != '\\':
+        regex = regex[:-1]
+    regex = regex + r'(\.(?P<emitter_format>json|xml))?$'
+    return url(regex, *args, **kwargs)

commit 93efd5931987a00c2bdf6db8bc51cf253805cb84

Fix XML output
Breakage introduced with previous commit 5c812d1.

api/resource.py
@@ -5,6 +5,7 @@
 
 from piston.resource import Resource as BaseResource
 from synnefo.helpers import parse_accept_header
+import synnefo.api.emitter # load our own Emitter
 
 class Resource(BaseResource):
     def determine_emitter(self, request, *args, **kwargs):
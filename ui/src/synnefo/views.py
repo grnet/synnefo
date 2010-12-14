6b4df678700fcddfdb9c78f4113c1df527237d69
Christodoulos Psaltis <cpsaltis@unweb.me>
2010-12-14 13:05:19 +0200

    two tabs for images in main

--- ui/src/synnefo/views.py
+++ ui/src/synnefo/views.py
@@ -24,10 +24,10 @@ def images(request):
     #conn = Driver(USER, SECRET) 
     #nodes = conn.list_images()     
     images = [
-              {'id': 'ubuntu-10.10-x86_64-server', 'title': 'Ubuntu 10.10 server 64bit', 'description': 'Apache, MySQL, php5 preinstalled', 'size': '834', 'logo':'/static/ubuntu.png'}, 
-              {'id': 'fedora-14-desktop', 'title': 'Fedora 14 desktop 32bit', 'description': 'Apache, MySQL, php5 preinstalled', 'size': '912', 'logo':'/static/fedora.png'}, 
-              {'id': 'windows7-pro', 'title': 'Windows 7 professional', 'description': 'MS Office 7 preinstalled', 'size': '8142', 'logo':'/static/windows.png'}, 
-              {'id': 'windows-xp', 'title': 'Windows XP', 'description': 'MS Office XP preinstalled', 'size': '6192', 'logo':'/static/windows.png'}, 
+              {'id': 'ubuntu-10.10-x86_64-server', 'type':'standard', 'title': 'Ubuntu 10.10 server 64bit', 'description': 'Apache, MySQL, php5 preinstalled', 'size': '834', 'logo':'/static/ubuntu.png'}, 
+              {'id': 'fedora-14-desktop', 'type':'standard', 'title': 'Fedora 14 desktop 32bit', 'description': 'Apache, MySQL, php5 preinstalled', 'size': '912', 'logo':'/static/fedora.png'}, 
+              {'id': 'windows7-pro', 'type':'standard', 'title': 'Windows 7 professional', 'description': 'MS Office 7 preinstalled', 'size': '8142', 'logo':'/static/windows.png'}, 
+              {'id': 'windows-xp', 'type':'custom', 'title': 'Windows XP', 'description': 'MS Office XP preinstalled', 'size': '6192', 'logo':'/static/windows.png'}, 
              ]
     return {'images': images}
 

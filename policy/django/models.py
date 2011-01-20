aadf97abb684c346658d1c6a4af7b46c7e2ada7e
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-01-20 18:38:52 +0200

    Updated django models (tested); added generated sql code for sqlite

--- policy/django/models.py
+++ policy/django/models.py
@@ -1,25 +1,31 @@
+from django.db import models
+
+class Limits(models.Model):
+    lim_id = models.IntegerField(primary_key=True)
+    lim_desc = models.CharField(max_length=45)
+
 class Users(models.Model):
+    user_id = models.IntegerField(primary_key=True)
     user_name = models.CharField(max_length=255)
     user_credit = models.IntegerField()
     user_quota = models.IntegerField()
     user_created = models.DateField()
     limits = models.ManyToManyField(Limits, through='UserLimit')
 
-class Limits(models.Model):
-    lim_desc = models.CharField(max_length=45)
-
 class UserLimit(models.Model):
-    lim_id = models.ForeignKey(Limits)
-    user_id = models.ForeignKey(Users)
+    lim_id = models.ForeignKey(Limits, primary_key=True)
+    user_id = models.ForeignKey(Users, primary_key=True)
     ul_value = models.IntegerField()
 
 class Flavor(models.Model):
-	flv_desc = models.CharField(max_length=255)
-	flv_cost_active = models.IntegerField()
-	flv_cost_inactive = models.IntegerField()
-	flv_detailed = models.CharField(max_length=1000)
+    flv_id = models.IntegerField(primary_key=True)
+    flv_desc = models.CharField(max_length=255)
+    flv_cost_active = models.IntegerField()
+    flv_cost_inactive = models.IntegerField()
+    flv_detailed = models.CharField(max_length=1000)
 
 class VMachine(models.Model):
+    vm_id = models.IntegerField(primary_key=True)
     vm_alias = models.CharField(max_length=255)
     vm_created = models.DateTimeField()
     vm_state = models.IntegerField()
@@ -28,6 +34,7 @@ class VMachine(models.Model):
     flv_id = models.ForeignKey(Flavor)
 
 class ChargingLog(models.Model):
+    cl_id = models.IntegerField(primary_key=True)
     vm_id = models.ForeignKey(VMachine)
     cl_date = models.DateTimeField()
     cl_credit = models.IntegerField()

0c4fee8b1df35a142b532c9120ea9b9166d88eff
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-01-20 16:55:07 +0200

    Updated models for aquarium database (django)

--- policy/models.py
+++ policy/models.py
@@ -1,54 +1,34 @@
-class Resource(models.Model):
-	res_desc = models.CharField(max_length=255)
-	res_unitcost = models.IntegerField()
-	res_max = models.IntegerField()
-	res_quantum = models.IntegerField()
-
 class Users(models.Model):
-	user_name = models.CharField(max_length=255)
-	user_credit = models.IntegerField()
-	user_quota = models.IntegerField()
-	user_created = models.DateField()
-	resources = models.ManyToManyField(Resource, through='UserResource')
-	groups = models.ManyToManyField(Group, through='UserGroup')
-	
-class VMachine(models.Model):
-	vm_alias = models.CharField(max_length=255)
-	vm_created = models.DateTimeField()
-	vm_active = models.BooleanField()
-	vm_started = models.DateTimeField()
-	users = models.ManyToManyField(Users)
-	resources = models.ManyToManyField(Resource, through='VMResource')
-	
-class ChargingLog(models.Model):
-	vm_id = models.ForeignKey(VMachine)
-	cl_date = models.DateTimeField()
-	cl_credit = models.IntegerField()
-	cl_message = models.CharField(max_length=1000)
-	
-class Groups(models.Model):
-	gr_name = models.CharField(max_length=45)
-	gr_credit = models.IntegerField()
-	gr_quota = models.IntegerField()
-	gr_created = models.DateTimeField()
-	resources = models.ManyToManyField(Resource, through='GroupResource')
-	
-class UserGroup(models.Model):
-	user = models.ForeignKey(Users)
-	group = models.ForeignKey(Groups)
-	ug_credit = models.IntegerField()
+    user_name = models.CharField(max_length=255)
+    user_credit = models.IntegerField()
+    user_quota = models.IntegerField()
+    user_created = models.DateField()
+    limits = models.ManyToManyField(Limits, through='UserLimit')
 
-class UserResource(models.Model):
-	user = models.ForeignKey(Users)
-	resource = models.ForeignKey(Resource)
-	ur_value = models.IntegerField()
-	
-class GroupResource(models.Model):
-	group = models.ForeignKey(Groups)
-	resource = models.ForeignKey(Resource)
-	gr_resource = models.IntegerField()
-	
-class VMResource(models.Model):
-	vmachine = models.ForeignKey(VMachine)
-	resource = models.ForeignKey(Resource)
-	conf_value = models.IntegerField()
+class Limits(models.Model):
+    lim_desc = models.CharField(max_length=45)
+
+class UserLimit(models.Model):
+    lim_id = models.ForeignKey(Limits)
+    user_id = models.ForeignKey(Users)
+    ul_value = models.IntegerField()
+
+class Flavor(models.Model):
+	flv_desc = models.CharField(max_length=255)
+	flv_cost_active = models.IntegerField()
+	flv_cost_inactive = models.IntegerField()
+	flv_detailed = models.CharField(max_length=1000)
+
+class VMachine(models.Model):
+    vm_alias = models.CharField(max_length=255)
+    vm_created = models.DateTimeField()
+    vm_state = models.IntegerField()
+    vm_started = models.DateTimeField()
+    user_id = models.ForeignKey(Users)
+    flv_id = models.ForeignKey(Flavor)
+
+class ChargingLog(models.Model):
+    vm_id = models.ForeignKey(VMachine)
+    cl_date = models.DateTimeField()
+    cl_credit = models.IntegerField()
+    cl_message = models.CharField(max_length=1000)

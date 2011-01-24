4fe1c922f031ccac8379555c658e29d98d1e6bf5
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-01-24 19:39:51 +0200

    Implementation of breeder (update user credit according to a monthly rate)

---
+++ aquarium/breeder.py
@@ -0,0 +1,20 @@
+#
+#
+#
+
+from aquarium.models import *
+
+# main entry point
+def main():
+    all_users = OceanUser.objects.all()
+
+    for u in all_users:
+        if u.credit < u.quota:
+            u.credit = u.credit + u.monthly_rate
+        if u.credit > u.quota:
+            u.credit = u.quota
+        print "Add %d credits to %s. Total: %d" % ( u.monthly_rate, u.name, u.credit )
+        u.save()
+
+#
+main()

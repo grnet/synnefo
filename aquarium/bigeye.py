d875d175169384242c1b3c30a36c3fdc3bec55e6
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-01-24 21:23:19 +0200

    Add bigeye and testbigeye units (demos).

---
+++ aquarium/bigeye.py
@@ -0,0 +1,37 @@
+#
+# Run all the time, and wait for messages from ganeti, then update the database
+#
+
+import zmq
+from aquarium.models import *
+
+def main():
+	context = zmq.Context()
+	
+	sock = context.socket(zmq.SUB)
+	sock.connect('tcp://127.0.0.1:6666')
+	
+	# accept all messages
+	sock.setsockopt(zmq.SUBSCRIBE, '')
+	
+	while True:
+		message = sock.recv()
+		
+		# do something
+		if message == 'start':
+			all_machines = VirtualMachine.objects.all()
+			for vm in all_machines:
+				vm.state = 1
+				vm.save()
+				print "Changed stated of vm with name %s to RUNNING" % ( vm.name, )
+		elif message == 'stop':
+			all_machines = VirtualMachine.objects.all()
+			for vm in all_machines:
+				vm.state = 0
+				vm.save()
+				print "Changed stated of vm with name %s to STOPPED" % ( vm.name, )
+		elif message == 'quit':
+			print "Killing the bigeye"
+			break
+	
+	sock.close()

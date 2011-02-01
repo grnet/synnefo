36ef26a0498f32ef35c4df35f35134ff545723d4
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-02-01 09:23:50 +0200

    Refactored CreditAllocator and Introduced main

--- db/CreditAllocator.py
+++ db/CreditAllocator.py
@@ -10,8 +10,10 @@ from db.models import *
 
 from django.db.models import F
 
+import logging
+
 # main entry point
-def allocate_credit():
+def main():
     """
     This method allocates credits for the users according to their montly rate
     """
@@ -21,7 +23,9 @@ def allocate_credit():
 
     for user in user_list:
         user.allocateCredit()
-        print "Add %d credits to %s. Total: %d" % ( user.monthly_rate, user.name, user.credit )
+        logging.info("Add %d credits to %s. Total: %d" % ( user.monthly_rate, user.name, user.credit ))
         user.save()
 
-allocate_credit()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    sys.exit(main())

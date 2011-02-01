bc4afb9473f0707224c6b900d6dbf40da9348253
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-02-01 10:56:11 +0200

    * Import ganeti into django application * Add tests.py for automated testing through django

---
+++ ganeti/tests.py
@@ -0,0 +1,13 @@
+#
+# Unit Tests for db
+#
+# Provides automated tests for ganeti module
+#
+# Copyright 2010 Greek Research and Technology Network
+#
+
+import unittest
+
+class GanetiTestCase():
+    def testGanetiZMQD(self):
+        return

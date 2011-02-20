db218dc90adb40efcdb49896573faa04a445217f
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-02-20 23:26:53 +0200

    * Added AccountingLog entries to db_test_data.json * AccountingLogTestCase is corrected and works as intended

--- db/tests.py
+++ db/tests.py
@@ -80,68 +80,11 @@ class FlavorTestCase(TestCase):
 
 
 class AccountingLogTestCase(TestCase):
-    def setUp(self):
-        """Setup the test"""
-        userdj = User.objects.create_user('testuser','test','test2')
-        userdj.save()
-        
-        # add a user
-        user = SynnefoUser(pk=3, name='Test User', credit=100, monthly_rate=10)
-        user.created = datetime.datetime.now()
-        user.user = userdj
-        user.violations = 0
-        user.save()
-        
-        # add an Image
-        si = Image(name='Test Name')
-        si.updated = datetime.datetime.now()
-        si.created = datetime.datetime.now()
-        si.state = 'ACTIVE'
-        si.owner = user
-        si.description = 'testing 1.2.3'
-        si.save()
-        
-        # add a Flavor
-        flavor = Flavor(pk=11, cpu=10, ram=10, disk=10)
-        flavor.save()
-        
-        # Now, add a VM
-        vm = VirtualMachine(pk=10)
-        vm.created = datetime.datetime.now()
-        vm.charged = datetime.datetime.now() - datetime.timedelta(hours=1)
-        vm.hostid = 'testhostid'
-        vm.server_label = 'agreatserver'
-        vm.image_version = '1.0.0'
-        vm.ipfour = '127.0.0.1'
-        vm.ipsix = '2001:0db8:85a3:0000:0000:8a2e:0370:7334'
-        vm.flavor = flavor
-        vm.sourceimage = si
-        vm.save()
-        
-        # Now add the log entries
-        alog = AccountingLog()
-        alog.vm = vm
-        alog.date = datetime.datetime(year=2010, month=01, day=01)
-        alog.state = 'STARTED'
-        alog.save()
-        
-        alog = AccountingLog()
-        alog.vm = vm
-        alog.date = datetime.datetime(year=2011, month=02, day=01)
-        alog.state = 'STOPPED'
-        alog.save()
-        
-    def tearDown(self):
-        """Cleaning up the data"""
-        user = User.objects.get(username='testuser')
-        user.delete()
-        
-        flavor = Flavor.objects.get(pk=11)
-        flavor.delete()
+    fixtures = [ 'db_test_data' ]
                 
     def test_accounting_log(self):
         """Test the Accounting Log unit method"""
-        vm = VirtualMachine.objects.get(pk=10)
+        vm = VirtualMachine.objects.get(pk=1001)
         
         # get all entries, should be 2
         entries = AccountingLog.get_log_entries(vm, datetime.datetime(year=2009, month=01, day=01))

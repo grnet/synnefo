f5e24ad4d500a460eda9afebe0ac5954c41e845a
Georgios Gousios <gousiosg@gmail.com>
2011-04-01 09:52:03 +0300

    Move authentication tests and code to logic

--- logic/tests.py
+++ logic/tests.py
@@ -99,3 +99,35 @@ class DebitAccountTestCase(TestCase):
         s_user = SynnefoUser.objects.get(pk=30000)
 
         self.assertEqual(0, s_user.credit, 'SynnefoUser (pk=30000) should have zero credits (%d)' % ( s_user.credit, ))
+
+class AuthTestCase(TestCase):
+    fixtures = ['api_test_data', 'auth_test_data']
+    apibase = '/api/v1.0'
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_auth_headers(self):
+        """ test whether the authentication mechanism sets the correct headers
+        """
+        #Check with non-existing user
+        response = self.client.get( self.apibase + '/servers', {},
+                                   **{'X-Auth-User':'notme',
+                                      'X-Auth-Key':'0xdeadbabe'})
+        self.assertEquals(response.status_code, 401)
+
+        #Check with existing user
+        response = self.client.get( self.apibase + '/', {},
+                                   **{'X-Auth-User':'testuser',
+                                      'X-Auth-Key':'testuserpasswd'})
+        self.assertEquals(response.status_code, 204)
+        self.assertNotEqual(response['X-Auth-Token'], None)
+        self.assertEquals(response['X-Server-Management-Url'], '')
+        self.assertEquals(response['X-Storage-Url'], '')
+        self.assertEquals(response['X-CDN-Management-Url'], '')
+
+        #Check access now that we do have an auth token
+        token = response['X-Auth-Token']
+        response = self.client.get (self.apibase + '/servers/detail', {},
+                                   **{'X-Auth-Token': token})
+        self.assertEquals(response.status_code, 200)

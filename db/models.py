98ab0687b9b744c57aeb2639a432d4507bd2c7d8
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2011-04-12 16:23:51 +0300

    Clean-up branch from migrations    new_south_test_field = models.IntegerField(default=1)    new_south_test_field = models.IntegerField(default=1)

--- db/models.py
+++ db/models.py
@@ -7,10 +7,9 @@ import datetime
 
 class SynnefoUser(models.Model):
     name = models.CharField('Synnefo Username', max_length=255)
-    wallet = models.IntegerField('Credit Balance')
+    credit = models.IntegerField('Credit Balance')
     created = models.DateTimeField('Time of creation', auto_now_add=True)
     updated = models.DateTimeField('Time of last update', auto_now=True)
-    new_south_test_field = models.IntegerField(default=1)
 
     class Meta:
         verbose_name = u'Synnefo User'

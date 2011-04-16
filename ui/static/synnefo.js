2604e505889d908cd738309383f26cdf07045d6b
Christodoulos Psaltis <cpsaltis@unweb.me>
2011-04-16 15:17:57 +0300

    Welcome screen appears based on serverlength counter, Refs #279

--- ui/static/synnefo.js
+++ ui/static/synnefo.js
@@ -1,6 +1,7 @@
 var flavors = [], images = [], servers = [], disks = [], cpus = [], ram = [];
 var changes_since = 0, deferred = 0, update_request = false, load_request = false, pending_actions = [];
 var API_URL = "/api/v1.1";
+var serverlength = 0;
 
 function ISODateString(d){
     //return a date in an ISO 8601 format using UTC. 
@@ -71,6 +72,7 @@ function update_confirmations(){
 }
 
 function list_view() {
+    serverlength = 0;  // reset server length
 	changes_since = 0; // to reload full list
 	pending_actions = []; // clear pending actions
 	update_confirmations();
@@ -101,6 +103,7 @@ function list_view() {
 }
 
 function standard_view() {
+    serverlength = 0; // reset server length
 	changes_since = 0; // to reload full list
 	pending_actions = []; // clear pending actions
 	update_confirmations();

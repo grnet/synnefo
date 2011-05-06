c7c4d304b626c16dce8033d81c2abb6e0547844e
Markos Gogoulos <mgogoulos@unweb.me>
2011-05-06 16:57:06 +0300

    fixes typo with previous commit
    regarding the ui auth_token

--- ui/static/synnefo.js
+++ ui/static/synnefo.js
@@ -4,7 +4,7 @@ var API_URL = "/api/v1.1";
 
 //FIXME: sends a fake HTTP_AUTH_TOKEN
     $.ajaxSetup({
-        'beforeSend': function(xhr) {xhr.setRequestHeader("X_Auth_Token", "46e427d657b20defe352804f0eb6f8a2")}
+        'beforeSend': function(xhr) {xhr.setRequestHeader("X-Auth-Token", "46e427d657b20defe352804f0eb6f8a2")}
     });
 
 

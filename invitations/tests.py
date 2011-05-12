791a9f323c8dd467c5b9e2235df2c1806f0da0a9
Georgios Gousios <gousiosg@gmail.com>
2011-05-12 23:17:59 +0300

    Test for the invitation add method

---
+++ invitations/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.test.client import Client
+
+import invitations
+from synnefo.db.models import SynnefoUser
+
+
+class InvitationsTestCase(TestCase):
+
+    token = '46e427d657b20defe352804f0eb6f8a2'
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_add_invitation(self):
+        source = SynnefoUser.objects.filter(auth_token = self.token)
+        invitations.add_invitation(source, "Test", "test@gmail.com")
+
+        added_user = SynnefoUser.objects.filter(name = "Test",
+                                                uniq = "test@gmail.com")
+
+        self.assertNotEquals(added_user, None)

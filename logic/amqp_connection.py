888b7a6186adf71b4b711cd7313e7dcc4546dd6c
Georgios Gousios <gousiosg@gmail.com>
2011-05-18 14:10:34 +0300

    Persistent connector to AMQP
    Maintains an open connection per running interpreter, through a global static variable

--- logic/amqp_connection.py
+++ logic/amqp_connection.py
@@ -1,20 +1,42 @@
 import time
+import socket
 from amqplib import client_0_8 as amqp
 from django.conf import settings
 
-conn = None
+_conn = None
+_chan = None
 
-class AMQPConnection:
+def _connect():
+    global _conn, _chan
+    while _conn == None:
+        try:
+            _conn = amqp.Connection(host=settings.RABBIT_HOST,
+                                   userid=settings.RABBIT_USERNAME,
+                                   password=settings.RABBIT_PASSWORD,
+                                   virtual_host=settings.RABBIT_VHOST)
+        except socket.error:
+            time.sleep(1)
+    _chan = _conn.channel()
 
-    def connect(self):
-        while conn == None:
-            try:
-                conn = amqp.Connection(host=settings.RABBIT_HOST,
-                               userid=settings.RABBIT_USERNAME,
-                               password=settings.RABBIT_PASSWORD,
-                               virtual_host=settings.RABBIT_VHOST)
-            except socket.error:
-                time.sleep(1)
 
-    
+def send(payload, exchange, key):
+    global _conn, _chan
+    msg = amqp.Message(payload)
+    msg.properties["delivery_mode"] = 2  # Persistent
 
+    while True:
+       try:
+           _chan.basic_publish(msg,
+                               exchange=exchange,
+                               routing_key=key)
+           return
+       except socket.error:
+           #logger.exception("Server went away, reconnecting...")
+           _connect()
+       except Exception as e:
+           #self.logger.exception("Caught unexpected exception (msg: %s)", msg)
+           _connect()
+
+
+def __init__():
+    _connect()

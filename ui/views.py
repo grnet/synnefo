611a373053f6c677047caa14f871b698c4e3045d
Christodoulos Psaltis <cpsaltis@unweb.me>
2011-06-28 17:02:00 +0300

    Change page title to ~okeanos, Refs. #699

--- ui/views.py
+++ ui/views.py
@@ -1,18 +1,18 @@
 # Copyright 2011 GRNET S.A. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following
 # conditions are met:
-# 
+#
 #   1. Redistributions of source code must retain the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer.
-# 
+#
 #   2. Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -25,12 +25,12 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and
 # documentation are those of the authors and should not be
 # interpreted as representing official policies, either expressed
 # or implied, of GRNET S.A.
-# 
+#
 import os
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -52,7 +52,7 @@ def template(name, context):
 
 def home(request):
     context = {'timeout': TIMEOUT,
-               'project': '+nefo',
+               'project': '~okeanos',
                'request': request,
                'current_lang': get_language() or 'en',
                'update_interval': UPDATE_INTERVAL,

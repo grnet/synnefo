fb95352532745854aeb8695f609b223a4cca1a7f
Vangelis Koukis <vkoukis@cslab.ece.ntua.gr>
2011-06-29 13:07:35 +0300

    Drop Django dependency in the Synnefo Ganeti hook
    Drop all dependencies on Django in the Synnefo Ganeti hook.
    
      * Include a new bash script, which sets PYTHONPATH appropriately
      * Update README.deploy, include instructions on setting
        SYNNEFO_PROJECT_DIR inside this wrapper.
    
    Refs #691

--- ganeti/snf-ganeti-hook.py
+++ ganeti/snf-ganeti-hook.py
@@ -14,40 +14,16 @@ set by Ganeti.
 
 """
 import logging
-from django.core.management import setup_environ
 
 import sys
 import os
 
-# Discover the path for the Synnefo project.
-#
-# IMPORTANT:
-# This assumes this script has been *linked* into the /etc/ganeti/hooks
-# directory on the master and nodes, and that it lives in a ganeti/ directory
-# under the Synnefo project root.
-
+# IMPORTANT: PYTHONPATH must contain the parent of the Synnefo project root.
 try:
-    target = os.readlink(sys.argv[0])
-except OSError:
-    target = sys.argv[0]
-target_script = os.path.abspath(target)
-target_dirname = os.path.dirname(target_script)
-
-if os.path.split(target_dirname)[1] != "ganeti":
-    raise Exception, "Unexpected location for the Synnefo Ganeti hook, " \
-        "cannot determine Synnefo project root.\n" \
-        "This script run as: %s\nLocation determined to be at: %s\n" \
-        "Script in %s, not under ganeti/ directory." % \
-        (sys.argv[0], target_script, target_dirname)
-
-# Add the parent of the project root to sys.path (for Python imports),
-# then load Django settings.
-# FIXME: Why do import references start at synnefo.* ?
-synnefo_project_root = os.path.split(target_dirname)[0]
-sys.path.append(os.path.join(synnefo_project_root, '..'))
-
-import synnefo.settings as settings
-setup_environ(settings)
+    import synnefo.settings as settings
+except ImportError:
+    raise Exception("Cannot import settings, make sure PYTHONPATH contains "
+                    "the parent directory of the Synnefo Django project.")
 
 # A hook runs either in the "pre" or "post" phase of a Ganeti operation.
 # Possible values for the Ganeti operation are "instance-start",

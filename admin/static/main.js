1b8c4d84451e81a0f9d2cb80d405fe80aec5c1c5
Giorgos Verigakis <verigak@gmail.com>
2011-08-31 17:47:17 +0300

    Add metadata support in image tab
    Refs #849

--- admin/static/main.js
+++ admin/static/main.js
@@ -1,3 +1,17 @@
 $(function() {
-	$("table.id-sorted").tablesorter({ sortList: [[0, 0]] });
+	$('table.id-sorted').tablesorter({ sortList: [[0, 0]] });
+
+	$('tr.row-template').hide();
+});
+
+$('.add-row').live('click', function() {
+	template = $('tr.row-template');
+	row = template.clone();
+	row.removeClass('row-template');
+	row.show();
+	template.parent().append(row);
+});
+
+$('.delete-row').live('click', function() {
+	$(this).parents('tr').remove();
 });

6a472b269d60b09e9853a6244f4bb5c44b61f953
Kostas Papadimitriou <kpap@grnet.gr>
2011-10-19 12:52:26 +0300

    Updated tests

--- userdata/tests.py
+++ userdata/tests.py
@@ -9,6 +9,7 @@ from django.test import TestCase
 from django.conf import settings
 from django.test.client import Client
 from django.core.urlresolvers import clear_url_caches
+from django.utils import simplejson as json
 
 from synnefo.userdata.models import User
 from synnefo.userdata.models import *
@@ -37,13 +38,36 @@ class TestRestViews(TestCase):
                 content="content1")
 
         resp = self.client.get("/keys/")
-        self.assertEqual(resp.content, """[{"content": "content1", "id": 1, "name": "key pair 1"}]""")
+        self.assertEqual(resp.content, """[{"content": "content1", "uri": "/keys/1/", "name": "key pair 1", "id": 1}]""")
 
         PublicKeyPair.objects.create(user=self.user, name="key pair 2",
                 content="content2")
 
         resp = self.client.get("/keys/")
-        self.assertEqual(resp.content, """[{"content": "content1", "id": 1, "name": "key pair 1"}, {"content": "content2", "id": 2, "name": "key pair 2"}]""")
+        self.assertEqual(resp.content, """[{"content": "content1", "uri": "/keys/1/", "name": "key pair 1", "id": 1}, {"content": "content2", "uri": "/keys/2/", "name": "key pair 2", "id": 2}]""")
 
     def test_keys_resourse_get(self):
-        pass
+        resp = self.client.get("/keys/1/")
+        self.assertEqual(resp.status_code, 404)
+
+        # create a public key
+        PublicKeyPair.objects.create(user=self.user, name="key pair 1",
+                content="content1")
+        resp = self.client.get("/keys/1/")
+        self.assertEqual(resp.content, """{"content": "content1", "uri": "/keys/1/", "name": "key pair 1", "id": 1}""")
+
+        # update
+        resp = self.client.post("/keys/1/", json.dumps({'name':'key pair 1 new name'}),
+                content_type='application/json')
+        pk = PublicKeyPair.objects.get(pk=1)
+        self.assertEqual(pk.name, "key pair 1 new name")
+
+        # delete
+        resp = self.client.delete("/keys/1/")
+        self.assertEqual(PublicKeyPair.objects.count(), 0)
+
+        resp = self.client.get("/keys/1/")
+        self.assertEqual(resp.status_code, 404)
+
+        resp = self.client.get("/keys/")
+        self.assertEqual(resp.content, "[]")

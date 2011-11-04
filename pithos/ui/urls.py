b590fb4654594cb0e9dd12aed3b4385de9d242e4
Giorgos Verigakis <verigak@gmail.com>
2011-11-04 17:09:59 +0200

    Add a feedback submission target
    Uses 'feedback-msg' and 'feedback-data' fields.
    
    An update to settings is required.

--- pithos/ui/urls.py
+++ pithos/ui/urls.py
@@ -1,18 +1,18 @@
 # Copyright 2011 GRNET S.A. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following
 # conditions are met:
-# 
+#
 #   1. Redistributions of source code must retain the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer.
-# 
+#
 #   2. Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -25,19 +25,14 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and
 # documentation are those of the authors and should not be
 # interpreted as representing official policies, either expressed
 # or implied, of GRNET S.A.
 
-from django.conf.urls.defaults import include, patterns
+from django.conf.urls.defaults import *
 
-
-urlpatterns = patterns('',
-    (r'^v1(?:$|/)', include('pithos.api.urls')),
-    (r'^v1\.0(?:$|/)', include('pithos.api.urls')),
-    (r'^public(?:$|/)', include('pithos.public.urls')),
-    (r'^im/', include('pithos.im.urls')),
-    (r'^ui', include('pithos.ui.urls'))
+urlpatterns = patterns('pithos.ui.views',
+    (r'^/feedback$', 'send_feedback'),
 )
--- pithos/ui/views.py
+++ pithos/ui/views.py
@@ -1,18 +1,18 @@
 # Copyright 2011 GRNET S.A. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following
 # conditions are met:
-# 
+#
 #   1. Redistributions of source code must retain the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer.
-# 
+#
 #   2. Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -25,19 +25,36 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-# 
+#
 # The views and conclusions contained in the software and
 # documentation are those of the authors and should not be
 # interpreted as representing official policies, either expressed
 # or implied, of GRNET S.A.
 
-from django.conf.urls.defaults import include, patterns
+import json
+
+from django.conf import settings
+from django.core.mail import send_mail
+from django.http import Http404, HttpResponse
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext as _
 
 
-urlpatterns = patterns('',
-    (r'^v1(?:$|/)', include('pithos.api.urls')),
-    (r'^v1\.0(?:$|/)', include('pithos.api.urls')),
-    (r'^public(?:$|/)', include('pithos.public.urls')),
-    (r'^im/', include('pithos.im.urls')),
-    (r'^ui', include('pithos.ui.urls'))
-)
+def send_feedback(request):
+    if request.method != 'POST':
+        raise Http404
+    if not request.user:
+        return HttpResponse('Unauthorized', status=401)
+    
+    subject = _("Feedback from Pithos")
+    from_email = settings.FEEDBACK_FROM_EMAIL
+    recipient_list = [settings.FEEDBACK_CONTACT_EMAIL]
+    content = render_to_string('feedback_mail.txt', {
+                'message': request.POST.get('feedback-msg'),
+                'data': request.POST.get('feedback-data'),
+                'request': request})
+    
+    send_mail(subject, content, from_email, recipient_list)
+    
+    resp = json.dumps({'status': 'send'})
+    return HttpResponse(resp)

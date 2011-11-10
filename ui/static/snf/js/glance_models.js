e69fb27b950a8bf34ac6e73ccc61e430afd0cc62
Kostas Papadimitriou <kpap@grnet.gr>
2011-11-10 18:24:32 +0200

    Created glance image model/collection objects

---
+++ ui/static/snf/js/glance_models.js
@@ -0,0 +1,35 @@
+;(function(root){
+    
+    // root
+    var root = root;
+    
+    // setup namepsaces
+    var snf = root.synnefo = root.synnefo || {};
+    var glance = snf.glance = snf.glance || {};
+    var models = glance.models = glance.models || {}
+    var storage = glance.storage = glance.storage || {};
+    var util = snf.util = snf.util || {};
+
+    // shortcuts
+    var bb = root.Backbone;
+    var slice = Array.prototype.slice
+
+    // logging
+    var logger = new snf.logging.logger("SNF-MODELS");
+    var debug = _.bind(logger.debug, logger);
+
+    
+    models.Image = snf.models.Image.extend({
+        api_type: 'glance'
+    })
+
+    models.Images = snf.models.Images.extend({
+        api_type: 'glance'
+    })
+    
+    // storage initialization
+    snf.storage.glance = {};
+    snf.storage.glance.images = new models.Images();
+
+})(this);
+

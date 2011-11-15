74607baa3713f9649740a17ba71d7a0939b1c218
Kostas Papadimitriou <kpap@grnet.gr>
2011-11-15 17:35:52 +0200

    Updated userdata application tests

--- ui/userdata/tests.py
+++ ui/userdata/tests.py
@@ -38,6 +38,7 @@ from django.test.client import Client
 from django.core.urlresolvers import clear_url_caches
 from django.utils import simplejson as json
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 from synnefo.ui.userdata.models import User
 from synnefo.ui.userdata.models import *
@@ -54,58 +55,77 @@ class TestRestViews(TestCase):
     fixtures = ['users']
 
     def setUp(self):
-        settings.ROOT_URLCONF = 'synnefo.ui.userdata.urls'
         settings.SKIP_SSH_VALIDATION = True
-        clear_url_caches()
         self.client = AaiClient()
         self.user = User.objects.get(pk=1)
+        self.keys_url = reverse('keys_collection')
 
     def test_keys_collection_get(self):
-        resp = self.client.get("/keys")
+        resp = self.client.get(self.keys_url)
         self.assertEqual(resp.content, "[]")
 
         PublicKeyPair.objects.create(user=self.user, name="key pair 1",
                 content="content1")
 
-        resp = self.client.get("/keys")
-        self.assertEqual(resp.content, """[{"content": "content1", "id": 1, "uri": "/keys/1", "name": "key pair 1", "fingerprint": ""}]""")
+        resp = self.client.get(self.keys_url)
+        resp_list = json.loads(resp.content);
+        exp_list = [{"content": "content1", "id": 1,
+                    "uri": self.keys_url + "/1", "name": "key pair 1",
+                    "fingerprint": "unknown fingerprint"}]
+        self.assertEqual(resp_list, exp_list)
 
         PublicKeyPair.objects.create(user=self.user, name="key pair 2",
                 content="content2")
 
-        resp = self.client.get("/keys")
-        self.assertEqual(resp.content, """[{"content": "content1", "id": 1, "uri": "/keys/1", "name": "key pair 1", "fingerprint": ""}, {"content": "content2", "id": 2, "uri": "/keys/2", "name": "key pair 2", "fingerprint": ""}]""")
+        resp = self.client.get(self.keys_url)
+        resp_list = json.loads(resp.content)
+        exp_list = [{"content": "content1", "id": 1,
+                     "uri": self.keys_url + "/1", "name": "key pair 1",
+                     "fingerprint": "unknown fingerprint"},
+                    {"content": "content2", "id": 2,
+                     "uri": self.keys_url + "/2",
+                     "name": "key pair 2",
+                     "fingerprint": "unknown fingerprint"}]
+
+        self.assertEqual(resp_list, exp_list)
 
     def test_keys_resourse_get(self):
-        resp = self.client.get("/keys/1")
+        resp = self.client.get(self.keys_url + "/1")
         self.assertEqual(resp.status_code, 404)
 
         # create a public key
         PublicKeyPair.objects.create(user=self.user, name="key pair 1",
                 content="content1")
-        resp = self.client.get("/keys/1")
-        self.assertEqual(resp.content, """{"content": "content1", "id": 1, "uri": "/keys/1", "name": "key pair 1", "fingerprint": ""}""")
+        resp = self.client.get(self.keys_url + "/1")
+        resp_dict = json.loads(resp.content);
+        exp_dict = {"content": "content1", "id": 1,
+                    "uri": self.keys_url + "/1", "name": "key pair 1",
+                    "fingerprint": "unknown fingerprint"}
+        self.assertEqual(resp_dict, exp_dict)
 
         # update
-        resp = self.client.put("/keys/1", json.dumps({'name':'key pair 1 new name'}),
-                content_type='application/json')
+        resp = self.client.put(self.keys_url + "/1",
+                               json.dumps({'name':'key pair 1 new name'}),
+                               content_type='application/json')
+
         pk = PublicKeyPair.objects.get(pk=1)
         self.assertEqual(pk.name, "key pair 1 new name")
 
         # delete
-        resp = self.client.delete("/keys/1")
+        resp = self.client.delete(self.keys_url + "/1")
         self.assertEqual(PublicKeyPair.objects.count(), 0)
 
-        resp = self.client.get("/keys/1")
+        resp = self.client.get(self.keys_url + "/1")
         self.assertEqual(resp.status_code, 404)
 
-        resp = self.client.get("/keys")
+        resp = self.client.get(self.keys_url)
         self.assertEqual(resp.content, "[]")
 
         # test rest create
-        resp = self.client.post("/keys", json.dumps({'name':'key pair 2',
-            'content':"""key 2 content"""}),
-                content_type='application/json')
+        resp = self.client.post(self.keys_url,
+                                json.dumps({'name':'key pair 2',
+                                            'content':"""key 2 content"""}),
+                                content_type='application/json')
         self.assertEqual(PublicKeyPair.objects.count(), 1)
         pk = PublicKeyPair.objects.get(pk=1)
         self.assertEqual(pk.name, "key pair 2")
@@ -115,7 +135,7 @@ class TestRestViews(TestCase):
         import base64
 
         # just test that
-        resp = self.client.post("/keys/generate")
+        resp = self.client.post(self.keys_url + "/generate")
         self.assertNotEqual(resp, "")
 
         data = json.loads(resp.content)
@@ -139,8 +159,9 @@ class TestRestViews(TestCase):
         new_key.save()
 
     def test_invalid_data(self):
-        resp = self.client.post("/keys", json.dumps({'content':"""key 2 content"""}),
-                content_type='application/json')
+        resp = self.client.post(self.keys_url,
+                                json.dumps({'content':"""key 2 content"""}),
+                                content_type='application/json')
 
         self.assertEqual(resp.status_code, 500)
         self.assertEqual(resp.content, """{"non_field_key": "__all__", "errors": """
@@ -149,12 +170,19 @@ class TestRestViews(TestCase):
         settings.USERDATA_MAX_SSH_KEYS_PER_USER = 2
 
         # test ssh limit
-        resp = self.client.post("/keys", json.dumps({'name':'key1', 'content':"""key 1 content"""}),
-                content_type='application/json')
-        resp = self.client.post("/keys", json.dumps({'name':'key1', 'content':"""key 1 content"""}),
-                content_type='application/json')
-        resp = self.client.post("/keys", json.dumps({'name':'key1', 'content':"""key 1 content"""}),
-                content_type='application/json')
+        resp = self.client.post(self.keys_url,
+                                json.dumps({'name':'key1',
+                                            'content':"""key 1 content"""}),
+                                content_type='application/json')
+        resp = self.client.post(self.keys_url,
+                                json.dumps({'name':'key1',
+                                            'content':"""key 1 content"""}),
+                                content_type='application/json')
+        resp = self.client.post(self.keys_url,
+                                json.dumps({'name':'key1',
+                                            'content':"""key 1 content"""}),
+                                content_type='application/json')
         self.assertEqual(resp.status_code, 500)
         self.assertEqual(resp.content, """{"non_field_key": "__all__", "errors": """
                                        """{"__all__": ["SSH keys limit exceeded."]}}""")
+

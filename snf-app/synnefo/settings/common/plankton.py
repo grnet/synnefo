3ebb5ba59ede60a54082df669332f9506a110c8f
Giorgos Verigakis <verigak@gmail.com>
2011-12-20 13:25:34 +0200

    Add some upgrade notes for Plankton

--- snf-app/synnefo/settings/common/plankton.py
+++ snf-app/synnefo/settings/common/plankton.py
@@ -5,16 +5,18 @@
 
 from os.path import join
 
+# Backend settings
 PITHOS_ROOT = '/usr/share/pithos'
 BACKEND_DB_MODULE = 'pithos.backends.lib.sqlalchemy'
 BACKEND_DB_CONNECTION = 'sqlite:///' + join(PITHOS_ROOT, 'backend.db')
 BACKEND_BLOCK_MODULE = 'pithos.backends.lib.hashfiler'
 BACKEND_BLOCK_PATH = join(PITHOS_ROOT, 'data/')
 
-# Default setting for new accounts.
-DEFAULT_QUOTA = 50 * 1024 * 1024 * 1024
+# Default settings for new accounts.
+DEFAULT_QUOTA = 50 * 1024**3
 DEFAULT_VERSIONING = 'auto'
 
+# The Pithos container where all images are stored
 PITHOS_IMAGE_CONTAINER = 'images'
 
 ALLOWED_DISK_FORMATS = ('diskdump', 'dump', 'extdump', 'lvm', 'ntfsclone',

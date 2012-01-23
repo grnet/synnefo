ea56f08cc09d31624c526b7b78fb61294f353502
Kostas Papadimitriou <kpap@grnet.gr>
2012-01-23 20:10:56 +0200

    Sane defaults for snf-pithos-backend db and data

--- snf-pithos-backend/pithos/backends/settings.py
+++ snf-pithos-backend/pithos/backends/settings.py
@@ -1,10 +1,14 @@
 # SQLAlchemy (choose SQLite/MySQL/PostgreSQL).
+from os.path import join
+
+BACKEND_PATH = '/usr/share/synnefo/pithos'
 BACKEND_DB_MODULE = 'pithos.backends.lib.sqlalchemy'
-BACKEND_DB_CONNECTION = 'sqlite:///' + join(PROJECT_PATH, 'backend.db')
+
+BACKEND_DB_CONNECTION = 'sqlite:////' + BACKEND_PATH + '/pithos-backend.db'
 
 # Block storage.
 BACKEND_BLOCK_MODULE = 'pithos.backends.lib.hashfiler'
-BACKEND_BLOCK_PATH = join(PROJECT_PATH, 'data/')
+BACKEND_BLOCK_PATH = join(BACKEND_PATH, 'data/')
 
 # Default setting for new accounts.
 BACKEND_QUOTA = 50 * 1024 * 1024 * 1024

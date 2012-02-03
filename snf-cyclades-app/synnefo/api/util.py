401460398d5126bb95e0c6b6e0ffca49ddf0e234
Giorgos Verigakis <verigak@gmail.com>
2012-02-03 11:37:21 +0200

    Bugfix
    Method name was get_image, not get_meta

--- snf-cyclades-app/synnefo/api/util.py
+++ snf-cyclades-app/synnefo/api/util.py
@@ -189,7 +189,7 @@ def get_image(image_id, owner):
 def get_backend_image(image_id, owner):
     backend = ImageBackend(owner.uniq)
     try:
-        image = backend.get_meta(image_id)
+        image = backend.get_image(image_id)
         if not image:
             raise ItemNotFound('Image not found.')
         return image

9b1587e3148a4634d6995381bb22a91fa533521f
Giorgos Verigakis <verigak@gmail.com>
2012-02-03 17:33:30 +0200

    Remove obsolete files that were restored due to merge

--- snf-cyclades-app/synnefo/helpdesk/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^token', 'synnefo.helpdesk.helpdesk.get_tmp_token'),
-    (r'^$', 'synnefo.helpdesk.helpdesk.index'),
-)

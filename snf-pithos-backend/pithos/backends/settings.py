5c93abdaf34f7e7b650f364fc33aa0c6431c2697
Vangelis Koukis <vkoukis@grnet.gr>
2012-02-03 14:59:28 +0200

    Use generic directory in backend settings
    Use generic "/srv/pithos" directory in default backend settings,
    to avoid specifying a location under /usr, which may be mounted
    read-only.

--- snf-pithos-backend/pithos/backends/settings.py
+++ snf-pithos-backend/pithos/backends/settings.py
@@ -1,7 +1,7 @@
 # SQLAlchemy (choose SQLite/MySQL/PostgreSQL).
 from os.path import join
 
-BACKEND_PATH = '/usr/share/synnefo/pithos'
+BACKEND_PATH = '/srv/pithos'
 BACKEND_DB_MODULE = 'pithos.backends.lib.sqlalchemy'
 
 BACKEND_DB_CONNECTION = 'sqlite:////' + BACKEND_PATH + '/pithos-backend.db'

43b0c92703acbe2fbcc76621236e47d8f7c894f4
Kostas Papadimitriou <kpap@grnet.gr>
2012-02-14 18:14:10 +0200

    Moved set_language view in webproject
    since the url that changes the user session language was moved in webproject
    as an app wide url, the view that gets bind to the url should exist also
    in webproject.

--- snf-webproject/synnefo/webproject/i18n.py
+++ snf-webproject/synnefo/webproject/i18n.py
@@ -1,18 +1,18 @@
 # Copyright 2011 GRNET S.A. All rights reserved.
-#
+# 
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following
 # conditions are met:
-#
+# 
 #   1. Redistributions of source code must retain the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer.
-#
+# 
 #   2. Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
-#
+# 
 # THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
 # OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -25,17 +25,32 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
+# 
 # The views and conclusions contained in the software and
 # documentation are those of the authors and should not be
 # interpreted as representing official policies, either expressed
 # or implied, of GRNET S.A.
+# 
+from django import http
+from django.conf import settings
+from django.utils.translation import activate, to_locale
+from django.utils.translation import check_for_language, get_language
 
-from django.conf.urls.defaults import *
-from synnefo.util.entry_points import extend_urls
-
-urlpatterns = patterns('',
-    (r'^lang/$', 'synnefo.webproject.i18n.set_language')
-)
-
-urlpatterns = extend_urls(urlpatterns, 'synnefo')
+def set_language(request):
+    """
+    Django's set_language function adapted to serve GET instead of POST
+    """
+    next = request.REQUEST.get('next', None)
+    if not next:
+        next = request.META.get('HTTP_REFERER', None)
+    if not next:
+        next = '/'
+    response = http.HttpResponseRedirect(next)
+    if request.method == 'GET':
+        lang_code = request.GET.get('l', None)
+        if lang_code and check_for_language(lang_code):
+            if hasattr(request, 'session'):
+                request.session['django_language'] = lang_code
+            else:
+                response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang_code)
+    return response

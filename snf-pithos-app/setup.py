769ea4557ad9f463c156625000e643623e07c8d8
Kostas Papadimitriou <kpap@grnet.gr>
2012-02-24 18:15:46 +0200

    Update required version of snf-pithos-backend

--- snf-pithos-app/setup.py
+++ snf-pithos-app/setup.py
@@ -66,7 +66,7 @@ CLASSIFIERS = []
 # Package requirements
 INSTALL_REQUIRES = [
     'snf-common>=0.9.0',
-    'snf-pithos-backend>=0.9.0',
+    'snf-pithos-backend>=0.9.1',
     'Django>=1.2, <1.3'
 ]
 

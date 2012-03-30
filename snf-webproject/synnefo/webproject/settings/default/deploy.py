68c6c939987c69a9b9579884320bc19a40500cd0
Kostas Papadimitriou <kpap@grnet.gr>
2012-03-30 17:05:32 +0300

    Set SESSION_SECURE_COOKIE to True by default

--- snf-webproject/synnefo/webproject/settings/default/deploy.py
+++ snf-webproject/synnefo/webproject/settings/default/deploy.py
@@ -4,6 +4,10 @@
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+# Use secure cookie for django sessions cookie, change this if you don't plan
+# to deploy applications using https
+SESSION_COOKIE_SECURE = True
+
 # You should always change this setting.
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'ly6)mw6a7x%n)-e#zzk4jo6f2=uqu!1o%)2-(7lo+f9yd^k^bg'

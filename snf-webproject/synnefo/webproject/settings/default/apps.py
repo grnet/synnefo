10a56e958e06f5772b8bd3de745bbd59c9eeb46a
Kostas Papadimitriou <kpap@grnet.gr>
2012-03-30 15:33:46 +0300

    Update webproject default settings
    - Removed settings that change based on entry_point hooks
    - Other minor default values improvements

--- snf-webproject/synnefo/webproject/settings/default/apps.py
+++ snf-webproject/synnefo/webproject/settings/default/apps.py
@@ -1,50 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright 2011 GRNET S.A. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or
-# without modification, are permitted provided that the following
-# conditions are met:
-#
-#   1. Redistributions of source code must retain the above
-#      copyright notice, this list of conditions and the following
-#      disclaimer.
-#
-#   2. Redistributions in binary form must reproduce the above
-#      copyright notice, this list of conditions and the following
-#      disclaimer in the documentation and/or other materials
-#      provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
-# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GRNET S.A OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# The views and conclusions contained in the software and
-# documentation are those of the authors and should not be
-# interpreted as representing official policies, either expressed
-# or implied, of GRNET S.A.
 
-#
-# Core Django settings
+# Core Django project settings
 ##################################
-
-INSTALLED_APPS = (
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.admin',
-    'south',
-    'synnefo.webproject'
-)
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
@@ -52,35 +9,18 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'django.core.context_processors.i18n',
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.media'
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'synnefo.lib.middleware.LoggingConfigMiddleware'
-)
-
+# This is a django project setting, do not change this unless you know
+# what you're doing
 ROOT_URLCONF = 'synnefo.webproject.urls'
 
+# Additional template dirs.
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates"
-    # or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '/usr/share/synnefo/templates/'
 )
 
 LANGUAGES = (
-  #('el', u'Ελληνικά'),
   ('en', 'English'),
 )
 
--- snf-webproject/synnefo/webproject/settings/default/deploy.py
+++ snf-webproject/synnefo/webproject/settings/default/deploy.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-#
-# Deployment
+# Deployment settings
 ##################################
 
 DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DEBUG = False
 
+# You should always change this setting.
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'ly6)mw6a7x%n)-e#zzk4jo6f2=uqu!1o%)2-(7lo+f9yd^k^bg'

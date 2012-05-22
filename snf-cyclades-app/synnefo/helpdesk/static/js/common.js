788a5ce7a946b3a8f5020a385803f80bca538c12
Olga Brani <olgabrani@grnet.gr>
2012-05-22 16:37:25 +0300

    Helpdesk style+ui update

--- snf-cyclades-app/synnefo/helpdesk/static/js/common.js
+++ snf-cyclades-app/synnefo/helpdesk/static/js/common.js
@@ -27,8 +27,69 @@ $(document).ready(function(){
 
   $win.on('scroll', processScroll)
 	
-	    
-     
+	  
+  
+  // hide/show expand/collapse 
+  
+  $('.subnav a').click(function(){
+  	$('.info-block-content, .show-hide-all').show();
+  })
+  
+  function  checkBadgeExpanded(el){
+  	if (el.hasClass('expanded')){
+  		el.html( txt_tab[1]);
+  	} else {
+  		el.html( txt_tab[0]);
+  	}
+  }
+  	
+  var txt_tab = ['+ Show Info','- Hide Info'];
+  var txt_all = ['+ Expand all','- Collapse all'];
+  
+  $('.show-hide-tabs span').html(txt_tab[0]); 	 
+  $('.show-hide-all span').html(txt_all[0]); 	 
+  
+  
+  $('.show-hide-all').click(function(){
+  	var badgeAll = $(this).children('span');	
+  	var tabs = $(this).parents('.info-block').find('.show-hide-tabs');
+  	badgeAll.toggleClass('open');
+  	
+  	if (badgeAll.hasClass('open')){
+  		badgeAll.html( txt_all[1]);
+  		tabs.each(function() {
+	  		$(this).next().show('slow');
+		    $(this).children('span').addClass('expanded');
+		    checkBadgeExpanded($(this).children('span'));
+	    });
+  		
+  		
+  	} else {
+  		badgeAll.html( txt_all[0]);
+  		tabs.each(function() {
+	  		$(this).next().hide('slow');
+		    $(this).children('span').removeClass('expanded');
+		    checkBadgeExpanded($(this).children('span'));
+	    });
+  	}
+	
+  	 
+  });   
+
+  		    
+  $('.show-hide-tabs').click(function(){	
+  	
+  	$(this).next().toggle('slow');
+  	var badge = $(this).children('span');
+  	badge.toggleClass('expanded');
+  	checkBadgeExpanded(badge);
+
+  }); 
+  
+  $('.info-block h3').click(function(){
+  	$(this).next('.info-block-content').toggle();
+  	$(this).prev('.show-hide-all').toggle();
+  })  
 	
 })
 

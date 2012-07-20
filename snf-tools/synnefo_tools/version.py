d9020dd748963e9bf00c58c617d0b657db24a4ee
John Giannelos <johngian@grnet.gr>
2012-07-20 16:59:53 +0300

    Initial commit to snf-tools

---
+++ snf-tools/synnefo_tools/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.10.0-rc2-38-g8252d64"
+__version_info__ = __version__.split(".")
+__version_vcs_info__ = ('snf-tools',
+ '8252d64fa057b4ebe9796a6dc4fd30d177f3ed11',
+ '3288',
+ 'v0.10.0-rc2-38-g8252d64')
+    

5c9f15129bf11adbd6cadf7aea9e36553126b2ab
Vangelis Koukis <vkoukis@grnet.gr>
2012-08-03 18:16:43 +0300

    Add ObjectPool class in synnefo.lib
    Add ObjectPool class in synnefo.lib.
    The ObjectPool class is an abstract pool of objects,
    based on Semaphore locks, as provided by the threading
    package.
    
    Tested to work with a gevent-monkey-patched threading package.
    Uncomment the relevant lines in pool/tests.py to test with greenlet
    contexts.

---
+++ snf-common/synnefo/lib/pool/__init__.py
@@ -0,0 +1,120 @@
+# Copyright 2011-2012 GRNET S.A. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or
+# without modification, are permitted provided that the following
+# conditions are met:
+#
+#   1. Redistributions of source code must retain the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer.
+#
+#   2. Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials
+#      provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GRNET S.A OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and
+# documentation are those of the authors and should not be
+# interpreted as representing official policies, either expressed
+# or implied, of GRNET S.A.
+
+
+"""Classes to support pools of arbitrary objects.
+
+The :class:`ObjectPool` class in this module abstracts a pool
+of arbitrary objects. Subclasses need to define the details regarding
+creation, destruction, allocation and release of their specific objects.
+
+"""
+
+# This should work under gevent, because gevent monkey patches 'threading'
+# if not, we can detect if running under gevent, e.g. using
+# if 'gevent' in sys.modules:
+#     from gevent.coros import Semaphore
+# else:
+#     from threading import Semaphore
+from threading import Semaphore, Lock
+
+
+__all__ = ['ObjectPool']
+
+
+class ObjectPool(object):
+    def __init__(self, size=None):
+        try:
+            self.size = int(size)
+            assert size >= 1
+        except:
+            raise ValueError("Invalid size for pool (positive integer "
+                             "required): %r" % (size,))
+
+        self._semaphore = Semaphore(size)  # Pool grows up to size
+        self._mutex = Lock()  # Protect shared _set oject
+        self._set = set()
+
+    def get(self, blocking=True):
+        """Get an object from the pool.
+
+        Get an object from the pool. Create a new object
+        if the pool has not reached its maximum size yet.
+
+        """
+        # timeout? supported by gevent and py3k variants of Semaphore
+        r = self._semaphore.acquire(blocking)
+        if not r:
+            return None
+        with self._mutex:
+            try:
+                obj = self._set.pop()
+            except KeyError:
+                obj = self._create()
+            except:
+                self._semaphore.release()
+                raise
+            assert len(self._set) <= self.size
+        # We keep_semaphore locked, put() will release it
+        return obj
+
+    def put(self, obj):
+        """Put an object back into the pool.
+
+        Return an object to the pool, for subsequent retrieval
+        by get() calls.
+
+        """
+        with self._mutex:
+            self._cleanup(obj)
+            self._set.add(obj)
+            assert len(self._set) <= self.size
+        self._semaphore.release()
+
+    def _create(self):
+        """Create a new object to be used with this pool.
+
+        Create a new object to be used with this pool,
+        should be overriden in subclasses.
+
+        """
+        raise NotImplementedError
+
+    def _cleanup(self, obj):
+        """Cleanup an object before being put back into the pool.
+
+        Cleanup an object before it can be put back into the pull,
+        ensure it is in a stable, reusable state.
+
+        """
+        raise NotImplementedError
---
+++ snf-common/synnefo/lib/pool/tests.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+#
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011 GRNET S.A. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or
+# without modification, are permitted provided that the following
+# conditions are met:
+#
+#   1. Redistributions of source code must retain the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer.
+#
+#   2. Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials
+#      provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GRNET S.A OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and
+# documentation are those of the authors and should not be
+# interpreted as representing official policies, either expressed
+# or implied, of GRNET S.A.
+#
+#
+
+"""Unit Tests for the pool classes in synnefo.lib.pool
+
+Provides unit tests for the code implementing pool
+classes in the synnefo.lib.pool module.
+
+"""
+
+# Uncomment the following to run a gevent version of the tests
+from gevent import monkey
+monkey.patch_all()
+
+import sys
+import time
+import threading
+
+from synnefo.lib.pool import ObjectPool
+
+# Use backported unittest functionality if Python < 2.7
+try:
+    import unittest2 as unittest
+except ImportError:
+    if sys.version_info < (2, 7):
+        raise Exception("The unittest2 package is required for Python < 2.7")
+    import unittest
+
+
+class NumbersPool(ObjectPool):
+    max = 0
+
+    def _create(self):
+        n = self.max
+        self.max += 1
+        return n
+
+    def _cleanup(self, obj):
+        pass
+
+
+class ObjectPoolTestCase(unittest.TestCase):
+    def test_create_pool_requires_size(self):
+        """Test __init__() requires valid size argument"""
+        self.assertRaises(ValueError, ObjectPool)
+        self.assertRaises(ValueError, ObjectPool, {"size": 'size10'})
+        self.assertRaises(ValueError, ObjectPool, {"size": 0})
+        self.assertRaises(ValueError, ObjectPool, {"size": -1})
+
+    def test_create_pool(self):
+        """Test pool creation works"""
+        pool = ObjectPool(100)
+        self.assertEqual(pool.size, 100)
+
+    def test_get_not_implemented(self):
+        """Test get() method not implemented in abstract class"""
+        pool = ObjectPool(100)
+        self.assertRaises(NotImplementedError, pool.get)
+
+    def test_put_not_implemented(self):
+        """Test put() method not implemented in abstract class"""
+        pool = ObjectPool(100)
+        self.assertRaises(NotImplementedError, pool.put, None)
+
+
+class NumbersPoolTestCase(unittest.TestCase):
+    N = 150
+    SEC = 0.5
+    maxDiff = None
+
+    def setUp(self):
+        self.numbers = NumbersPool(self.N)
+
+    def test_initially_empty(self):
+        """Test pool is empty upon creation"""
+        self.assertEqual(self.numbers._set, set([]))
+
+    def test_seq_allocate_all(self):
+        """Test allocation and deallocation of all pool objects"""
+        n = []
+        for _ in xrange(0, self.N):
+            n.append(self.numbers.get())
+        self.assertEqual(n, range(0, self.N))
+        for i in n:
+            self.numbers.put(i)
+        self.assertEqual(self.numbers._set, set(n))
+
+    def test_parallel_allocate_all(self):
+        """Allocate all pool objects in parallel"""
+        def allocate_one(pool, results, index):
+            n = pool.get()
+            results[index] = n
+
+        results = [None] * self.N
+        threads = [threading.Thread(target=allocate_one,
+                                    args=(self.numbers, results, i,))
+                   for i in xrange(0, self.N)]
+
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+        # This nonblocking pool get() should fail
+        self.assertIsNone(self.numbers.get(blocking=False))
+        self.assertEqual(sorted(results), range(0, self.N))
+
+    def test_parallel_get_blocks(self):
+        """Test threads block if no object left in the pool"""
+        def allocate_one_and_sleep(pool, sec, result, index):
+            n = pool.get()
+            time.sleep(sec)
+            result[index] = n
+            pool.put(n)
+
+        results = [None] * (2 * self.N + 1)
+        threads = [threading.Thread(target=allocate_one_and_sleep,
+                                    args=(self.numbers, self.SEC, results, i,))
+                   for i in xrange(0, 2 * self.N + 1)]
+
+        # This should take 3 * SEC seconds
+        start = time.time()
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        diff = time.time() - start
+        self.assertTrue(diff > 3 * self.SEC)
+        self.assertLess((diff - 3 * self.SEC) / 3 * self.SEC, .5)
+
+        # One number must have been used three times,
+        # all others must have been used once
+        freq = {}
+        for r in results:
+            freq[r] = freq.get(r, 0) + 1
+        self.assertTrue(len([r for r in results if freq[r] == 2]), self. N)
+        triples = [r for r in freq if freq[r] == 3]
+        self.assertTrue(len(triples), 1)
+        self.assertEqual(sorted(results),
+                         sorted(2 * range(0, self.N) + triples))
+
+
+if __name__ == '__main__':
+    unittest.main()

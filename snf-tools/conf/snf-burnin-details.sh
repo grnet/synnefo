dd62407c20e5403f12f7819b55444e4e0574e36a
John Giannelos <johngian@grnet.gr>
2012-09-10 18:18:34 +0300

    Fix performance issues in snf-burnin-details script

--- snf-tools/conf/snf-burnin-details.sh
+++ snf-tools/conf/snf-burnin-details.sh
@@ -3,20 +3,29 @@
 #Print the details for testcases that failed the last 30 minutes. 
 #Usage: ./snf-burnin-details.sh LOG_FOLDER
 
+path=$1
+flag=0
 curr=$(date -d "30 minutes ago" +%Y%m%d%H%M%S)
-for file in ${1}/*/*/detail* ; do
+for dir in `find ${path} -maxdepth 1 -type d -cmin -60`; do
 
-    x=`dirname $file`
-    y=`dirname $x`
-    d=`basename $y`
-
-    if (($d<$curr)); then
-    	if grep -E "(ERROR)|(FAILED)" "$file" >/dev/null; then
-    	    cat "$file"
-	    flag=1
-    	fi
+    if [ x"$dir" == "x${path}" ]; then
+	continue
     fi
+    for file in $dir/*/detail* ; do
+
+	x=`dirname $file`
+	y=`dirname $x`
+	d=`basename $y`
+
+	if (($d<$curr)); then
+	    if grep -E "(ERROR)|(FAILED)" "$file" >/dev/null; then
+		cat "$file"
+		flag=1
+	    fi
+	fi
+    done
 done
+exit
 
 if [ $flag -ge 1 ]; then 
     exit 1

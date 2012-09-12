1698c08c9fdb30f5f09177e7fff52429f613c7e9
Georgios D. Tsoukalas <gtsouk@cslab.ece.ntua.gr>
2012-09-12 14:11:08 +0300

    add quotaholder fixtures to distribution

--- setup.py
+++ setup.py
@@ -2,33 +2,37 @@
 from distutils.core import setup
 
 setup   (
-    name        =   'commissioning',
-    version     =   '0.3',
-    packages    =   [
-                        'commissioning',
-                        'commissioning.api',
-                        'commissioning.controllers',
-                        'commissioning.controllers.django_controller',
+    name            =   'commissioning',
+    version         =   '0.3',
+    packages        =   [
+            'commissioning',
+            'commissioning.api',
+            'commissioning.controllers',
+            'commissioning.controllers.django_controller',
 
-                        'commissioning.specs',
-                        'commissioning.physicals',
-                        'commissioning.physicals.fscrud',
+            'commissioning.specs',
+            'commissioning.physicals',
+            'commissioning.physicals.fscrud',
 
-                        'commissioning.clients',
-                        'commissioning.servers',
-                        'commissioning.servers.django_server',
-                        'commissioning.servers.django_server.server_app',
-                        'commissioning.servers.quotaholder',
-                        'commissioning.servers.quotaholder.quotaholder_django',
-                        'commissioning.servers.quotaholder.django_backend',
-			'commissioning.servers.fscrud',
-			'commissioning.servers.fscrud.fscrud_django',
+            'commissioning.clients',
+            'commissioning.servers',
+            'commissioning.servers.django_server',
+            'commissioning.servers.django_server.server_app',
+            'commissioning.servers.quotaholder',
+            'commissioning.servers.quotaholder.quotaholder_django',
+            'commissioning.servers.quotaholder.django_backend',
+            'commissioning.servers.fscrud',
+            'commissioning.servers.fscrud.fscrud_django',
 
-                        'commissioning.utils',
-                    ],
+            'commissioning.utils',
+    ],
 
-    scripts     =   [
-                        'commissioning/servers/quotaholder/quotaholder_django/quotaholder-manage',
-                        'commissioning/servers/fscrud/fscrud_django/fscrud-manage',
-                    ]
+    package_data    =   {
+            'commissioning.servers.quotaholder.django_backend': ['fixtures/*.json']
+    },
+
+    scripts         =   [
+            'commissioning/servers/quotaholder/quotaholder_django/quotaholder-manage',
+            'commissioning/servers/fscrud/fscrud_django/fscrud-manage',
+    ]
 )

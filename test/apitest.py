e254ab51caf234d3d0d29062bd36642fe3d6700e
Christos KK Loverdos <loverdos@gmail.com>
2012-10-08 18:32:48 +0300

    Fill skeletons of API tests

--- test/apitest.py
+++ test/apitest.py
@@ -41,9 +41,62 @@ class SimpleAPICall(unittest.TestCase):
 #        print 'In tearDown()'
         del self.qh
 
-    def testCreate(self):
+    def test_001_create_entity(self):
         rejected = self.qh.create_entity(context={},create_entity=[("pgerakios","system","key1","")])
         self.assertEqual(rejected,[])
+
+    def test_002_set_entity_key(self):
+        pass
+
+    def test_003_list_entities(self):
+        pass
+
+    def test_004_get_entity(self):
+        pass
+
+    def test_005_get_limits(self):
+        pass
+
+    def test_006_set_limits(self):
+        pass
+
+    def test_007_get_holding(self):
+        pass
+
+    def test_008_set_holding(self):
+        pass
+
+    def test_009_list_resources(self):
+        pass
+
+    def test_010_get_quota(self):
+        pass
+
+    def test_011_set_quota(self):
+        pass
+
+    def test_012_issue_commission(self):
+        pass
+
+    def test_013_accept_commission(self):
+        pass
+
+    def test_014_reject_commission(self):
+        pass
+
+    def test_015_get_pending_commissions(self):
+        pass
+
+    def test_016_resolve_pending_commissions(self):
+        pass
+
+    def test_017_release_entity(self):
+        pass
+
+    def test_018_get_timeline(self):
+        pass
+
+
 
 if __name__ == "__main__":
     unittest.main()

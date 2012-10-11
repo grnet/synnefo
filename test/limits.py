51f5ff5332b82dc68c271d22eb963eb5dbe3e99c
Christos KK Loverdos <loverdos@gmail.com>
2012-10-11 13:23:50 +0300

    Make limits.py runnable via python -m test.limits

--- test/limits.py
+++ test/limits.py
@@ -1,5 +1,5 @@
 from kkconfig import QHTestCase
-from kkconfig import new_quota_holder_client
+from kkconfig import run_test_case
 
 class LimitsTest(QHTestCase):
     def test_01_set_get(self):
@@ -22,7 +22,10 @@ class LimitsTest(QHTestCase):
         # GET
         limits = self.qh.get_limits(
             context = {},
-            get_limits = [policy]
+            get_limits = [(policy, )] # or is it just policy, i.e. no
         )
 
         self.assertTrue(len(limits) > 1)
+
+if __name__ == "__main__":
+    run_test_case(LimitsTest)

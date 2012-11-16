08418e4c8ff9623bd175deebac4b4559d3f00773
Olga Brani <olgabrani@grnet.gr>
2012-11-16 15:34:10 +0200

    Alter main menu

--- snf-astakos-app/astakos/im/api/__init__.py
+++ snf-astakos-app/astakos/im/api/__init__.py
@@ -178,21 +178,23 @@ def get_menu(request, with_extra_links=False, with_signout=True):
         append(item(url=absolute(request, reverse('edit_profile')),
                name="My account"))
         if with_extra_links:
+            """
             if user.has_usable_password() and user.provider in ('local', ''):
                 append(item(
                        url=absolute(request, reverse('password_change')),
                        name="Change password"))
+            """
             if EMAILCHANGE_ENABLED:
                 append(item(
                        url=absolute(request, reverse('email_change')),
                        name="Change email"))
+            """
             if INVITATIONS_ENABLED:
                 append(item(
                        url=absolute(request, reverse('invite')),
                        name="Invitations"))
-            append(item(
-                   url=absolute(request, reverse('feedback')),
-                   name="Feedback"))
+            """
+            
             append(item(
                    url=absolute(request, reverse('group_list')),
                    name="Groups",
@@ -210,7 +212,10 @@ def get_menu(request, with_extra_links=False, with_signout=True):
                                 name="Join"),)))
             append(item(
                    url=absolute(request, reverse('resource_list')),
-                   name="Resources"))
+                   name="Report"))
+            append(item(
+                   url=absolute(request, reverse('feedback')),
+                   name="Feedback"))
             """
             append(item(
                    url=absolute(request, reverse('billing')),

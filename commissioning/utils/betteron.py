bd5056442ddea162f706f1aa804a8803cf2a3892
root <root@dev84.dev.grnet.gr>
2012-11-16 13:10:45 +0200

    Use backported OrderedDict if not found.

--- commissioning/utils/betteron.py
+++ commissioning/utils/betteron.py
@@ -1,4 +1,7 @@
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    from commissioning.utils.ordereddict import OrderedDict
 from itertools import chain
 from cStringIO import StringIO
 

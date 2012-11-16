4ceee256ceb154f467566917b342ff8b85542d40
Olga Brani <olgabrani@grnet.gr>
2012-11-16 16:08:03 +0200

    bug fix

--- snf-astakos-app/astakos/im/templatetags/filters.py
+++ snf-astakos-app/astakos/im/templatetags/filters.py
@@ -170,11 +170,11 @@ def sizeof_fmt(num):
         exponent = min(int(log(num, 1024)), len(unit_list) - 1)
         quotient = float(num) / 1024**exponent
         unit, num_decimals = unit_list[exponent]
-        format_string = '{:.%sf} {}' % (num_decimals)
+        format_string = '{:.%sf} {0}' % (num_decimals)
         return format_string.format(quotient, unit)
     if num == 0:
         return '0 bytes'
     if num == 1:
         return '1 byte'
     else:
-       return ''
+       return '';

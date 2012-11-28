700545ab5f2382a59193cf864ea2ab1f23a34932
Kostas Papadimitriou <kpap@grnet.gr>
2012-11-28 12:01:25 +0200

    Fix handling of firewallProfile action
    Refs #3097

--- snf-cyclades-app/synnefo/api/servers.py
+++ snf-cyclades-app/synnefo/api/servers.py
@@ -448,6 +448,9 @@ def server_action(request, server_id):
     req = util.get_request_dict(request)
     log.debug('server_action %s %s', server_id, req)
 
+    # additional server actions
+    ARBITRARY_ACTIONS = ['console', 'firewallProfile']
+
     if len(req) != 1:
         raise faults.BadRequest("Malformed request")
 
@@ -457,7 +460,7 @@ def server_action(request, server_id):
 
     try:
         key = req.keys()[0]
-        if key != 'console':
+        if key not in ARBITRARY_ACTIONS:
             start_action(vm, key_to_action(key))
         val = req[key]
         assert isinstance(val, dict)
@@ -474,7 +477,7 @@ def key_to_action(key):
         return "STOP"
     if key == "delete":
         return "DESTROY"
-    if key == "console":
+    if key in ARBITRARY_ACTIONS:
         return None
     else:
         return key.upper()

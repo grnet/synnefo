14b82fa0e649e959922d881c2130510883ec423a
Dimitris Aragiorgis <dimara@grnet.gr>
2012-12-11 23:01:18 +0200

    Ensure autopkg not to mess up local repo
    Use add_cleanup, add_checkpoint (based on git reflog) and cleanup
    handler upon exit so that local src repo preserves its state after
    trying to build packages no matter if build process succeeds or not.
    
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- devtools/autopkg.sh
+++ devtools/autopkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-  echo "$0 local_branch upstream_branch debian_branch"
+if [ $# -ne 2 ]; then
+  echo "$0 upstream_branch debian_branch"
   exit 1
 fi
 
@@ -16,15 +16,46 @@ die()
     exit 1
 }
 
+cleanup()
+{
+    trap - EXIT
+
+    if [ ${#CLEANUP[*]} -gt 0 ]; then
+        LAST_ELEMENT=$((${#CLEANUP[*]}-1))
+        REVERSE_INDEXES=$(seq ${LAST_ELEMENT} -1 0)
+        for i in $REVERSE_INDEXES; do
+            local cmd=${CLEANUP[$i]}
+            $cmd
+        done
+    fi
+}
+
+add_cleanup() {
+    local cmd=""
+    for arg; do cmd+=$(printf "%q " "$arg"); done
+    CLEANUP+=("$cmd")
+}
+
+
+add_checkpoint()
+{
+    commit=$(git reflog | head -n1 | cut -f 1 -d " ")
+    add_cleanup git reset --hard $commit
+    LASTCHECKPOINT=$commit
+}
+
+CLEANUP=( )
+
 # The root of the git repository, no matter where we're called from
 TOPLEVEL="$(git rev-parse --show-toplevel)"
 CURRENT_BRANCH=$(parse_git_branch)
 
 LOCALBRANCH="$CURRENT_BRANCH"
-REMOTEUPSTREAM=$2
-REMOTEDEBIAN=$3
+REMOTEUPSTREAM=$1
+REMOTEDEBIAN=$2
 PKGAREA=~/packages
 BACKUPAREA=~/backup
+BUILDAREA=$(mktemp -d --tmpdir=/tmp build-area.XXX)
 
 PACKAGES="
   snf-astakos-app
@@ -39,6 +70,9 @@ PACKAGES="
 
 
 set -e
+trap cleanup EXIT
+
+cd "$TOPLEVEL"
 
 # Prerequisites: Test all important directories exist
 test -d "$PKGAREA" || die "Package area directory $PKGAREA missing"
@@ -49,26 +83,30 @@ dialog --help &>/dev/null || die "Could not run the 'dialog' utility"
 
 # Test all needed branches exist and can be checked out
 #TODO: check for up-to-date branches
-git checkout $REMOTEUPSTREAM || die "Could not checkout $REMOTEUPSTREAM"
-git checkout $REMOTEDEBIAN || die "Could not checkout $REMOTEDEBIAN"
-git checkout $LOCALBRANCH || die "Could not checkout $LOCALBRANCH"
+git fetch origin $REMOTEUPSTREAM
+git fetch origin $REMOTEDEBIAN
 
-# Move everything into a temp directory under /tmp
-# ACHTUNG: "origin" is now the original toplevel directory of the git repo
-TEMPAREA=$(mktemp -d)
-cd "$TEMPAREA"
-git clone "$TOPLEVEL" synnefo
-cd synnefo
-BUILDAREA="$TEMPAREA"/build-area
-
-echo "Will build packages under $TEMPAREA, from branch $LOCALBRANCH"
-echo "with upstream branch $REMOTEUPSTREAM, and debian branch $REMOTEDEBIAN"
+echo "##########################################################"
+echo "Will build packages"
+echo "under '$BUILDAREA',"
+echo "from branch '$LOCALBRANCH'"
+echo "with upstream branch '$REMOTEUPSTREAM',"
+echo "and debian branch '$REMOTEDEBIAN'"
+echo "##########################################################"
 echo "Press Enter to continue..."
 read
 
+add_checkpoint
+
 # Create a temporary debian branch to do everything
 TMPDEBIAN=$(mktemp -u debian.XXX)
-git checkout --track origin/$REMOTEDEBIAN -b $TMPDEBIAN
+git branch --track $TMPDEBIAN  origin/$REMOTEDEBIAN
+#add_cleanup git branch -D $TMPDEBIAN
+
+git checkout $TMPDEBIAN
+add_cleanup git checkout $LOCALBRANCH
+
+add_checkpoint
 
 # Whether we are in snapshot or release mode
 snap=false
@@ -94,11 +132,12 @@ version=$(IFS="()" ; read  x v x < debian/changelog  ; echo $v)
 if ! $snap; then
   git commit -s -a -m "Bump new upstream version"
   TAGFILE=$(mktemp -t tag.XXX)
+  add_cleanup rm $TAGFILE
   dialog --inputbox "New Debian Tag: " 5 30 "debian/$version" 2>$TAGFILE
   git tag $(<$TAGFILE)
+  add_cleanup git tag -d $(<$TAGFILE)
 fi
 
-mkdir "$BUILDAREA"
 
 for p in $PACKAGES; do
 
@@ -119,26 +158,16 @@ git-buildpackage --git-export-dir="$BUILDAREA" \
 # do some dirty backup
 # pkgarea might be needed by auto-deploy tool
 rm -f "$PKGAREA"/* || true
-cp -v "$BUILDAREA"/*deb "$PKGAREA"/ || true
+cp -v "$BUILDAREA"/* "$PKGAREA"/ || true
 cp -v "$BUILDAREA"/* "$BACKUPAREA"/ || true
 
 # Revert the changes, altough everything should have taken
 # place inside a temporary directory, and we can probably nuke everything
-git reset --hard HEAD
-if $snap; then
-  git checkout $LOCALBRANCH
-  git branch -D $TMPDEBIAN
-else
+if ! $snap; then
   # here we can push the commits to the remote debian branch as they are
-  echo "########### All OK #####################"
-  echo "IF you want to push the temp debian branch to upstream $REMOTEDEBIAN,"
-  echo "you may run this inside $TEMPAREA:"
+  echo "#################### All OK ###################"
   echo "git push --tags origin $TMPDEBIAN:$REMOTEDEBIAN"
-  echo "git checkout $LOCALBRANCH"
   echo
-  echo "Please also remember to remove $TEMPAREA."
-  echo
-  echo "############ Revert ######################"
-  echo "git tag -d " $(<$TAGFILE)
-  rm $TAGFILE
 fi
+
+exit 0

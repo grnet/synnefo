e0c3b5642f9b222da195e553e0472a45c8e9085e
Sofia Papagiannaki <papagian@gmail.com>
2012-12-11 12:02:01 +0200

    Set unique together resource name and service

--- snf-astakos-app/astakos/im/models.py
+++ snf-astakos-app/astakos/im/models.py
@@ -157,12 +157,15 @@ class ResourceMetadata(models.Model):
 
 
 class Resource(models.Model):
-    name = models.CharField(_('Name'), max_length=255, unique=True, db_index=True)
+    name = models.CharField(_('Name'), max_length=255)
     meta = models.ManyToManyField(ResourceMetadata)
     service = models.ForeignKey(Service)
     desc = models.TextField(_('Description'), null=True)
     unit = models.CharField(_('Name'), null=True, max_length=255)
     group = models.CharField(_('Group'), null=True, max_length=255)
+    
+    class Meta:
+        unique_together = ("name", "service")
 
     def __str__(self):
         return '%s%s%s' % (self.service, RESOURCE_SEPARATOR, self.name)

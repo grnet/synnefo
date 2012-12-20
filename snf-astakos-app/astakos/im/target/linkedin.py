2f560ffcf6af6a579e39e8c4ad602aa2036be63d
Kostas Papadimitriou <kpap@grnet.gr>
2012-12-20 12:24:02 +0200

    Show message after login with third party provider

--- snf-astakos-app/astakos/im/target/linkedin.py
+++ snf-astakos-app/astakos/im/target/linkedin.py
@@ -169,6 +169,7 @@ def authenticated(
                                     user,
                                     request.GET.get('next'),
                                     'renew' in request.GET)
+            messages.success(request, _(astakos_messages.LOGIN_SUCCESS))
             response.set_cookie('astakos_last_login_method', 'linkedin')
             return response
         else:

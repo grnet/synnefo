46087a08ccc9d0e3709d043e40165e91ee880f1f
Kostas Papadimitriou <kpap@grnet.gr>
2013-01-03 16:54:13 +0200

    Update package dependencies

--- snf-astakos-app/setup.py
+++ snf-astakos-app/setup.py
@@ -78,6 +78,7 @@ INSTALL_REQUIRES = [
     'South>=0.7, <=0.7.3',
     'httplib2>=0.6.0',
     'snf-common>=0.9.0',
+    'django-tables2',
     'recaptcha-client>=1.0.5',
     'django-ratelimit==0.1',
     'celery',

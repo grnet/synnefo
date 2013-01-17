5f0eba383b4afbb1bc2a09823c3ab8c04fb120c6
Olga Brani <olgabrani@grnet.gr>
2013-01-17 11:21:16 +0200

    New login form improvements

--- snf-astakos-app/astakos/im/static/im/js/common.js
+++ snf-astakos-app/astakos/im/static/im/js/common.js
@@ -275,6 +275,7 @@ $(document).ready(function() {
 	newPassword2Div = $('#id_new_password2').parents('.form-row');
 	emailCheck = $('#id_change_email').parents('.form-row');
 	passwordCheck = $('#id_change_password').parents('.form-row');
+	authTokenDiv = $('#id_auth_token').parents('.form-row');
 	
 	if ( newEmailDiv.length>0  ){ 
 		emailDiv.addClass('form-following');
@@ -287,7 +288,7 @@ $(document).ready(function() {
 	
 	emailDiv.find('span.extra-img').attr('id','email-span');
 	oldPasswordDiv.find('span.extra-img').attr('id','password-span');
-	
+	authTokenDiv.find('span.extra-img').attr('id','token-span');
 	// Default hidden fields
 	
 	newEmailDiv.hide();
@@ -302,9 +303,28 @@ $(document).ready(function() {
 	newPassword1Div.addClass('password-span');
 	newPassword2Div.addClass('password-span');
 	
+	// If errors show fields
+	
+	
+	if ($('input#id_change_password:checkbox').attr('checked')) {
+		oldPasswordDiv.find('input').focus();
+		newPassword1Div.show();
+		newPassword2Div.show();
+		$('.form-following #password-span').parents('.form-row').addClass('open');
+	}; 
+	
+	
+	
+	if ($('input#id_change_password:checkbox').attr('checked')) {
+		newEmailDiv.show();
+		
+		$('.form-following #email-span').parents('.form-row').addClass('open');
+	}; 
+	
 	// Email, Password forms
 	
 	$('.form-following .extra-img').click(function(e){
+		
 		$(this).parents('.form-row').toggleClass('open');
 		id = $(this).attr('id');
 		$('.form-row').each(function() {
@@ -313,27 +333,42 @@ $(document).ready(function() {
 			}
 		}); 
 	 
+	 	
 	 	if ( !($(this).parents('.form-row').hasClass('open')) ){
 	 		$('.form-row').each(function() {
 				if( $(this).hasClass(id) ) {
 					console.info($(this).find('input[type="text"]'));
 					$(this).find('input').val('');
+					$(this).removeClass('with-errors');
+					$(this).find('.form-error').hide();
 				}
 			}); 
+	 	} 	else {
+	 		// focus on first input
+	 		if ( id == 'email-span') { newEmailDiv.find('input').focus(); } 
+	 		if ( id == 'password-span') { oldPasswordDiv.find('input').focus(); }
 	 	}
 	});
 	
-	
+	//  check uncheck checkbox
 	$('#email-span').click(function(){ 
        var $checkbox = $('input#id_change_email:checkbox');
        $checkbox.attr('checked', !$checkbox.attr('checked'));
  	});
 	
-	
+	//  check uncheck checkbox
 	$('#password-span').click(function(){ 
        var $checkbox = $('input#id_change_password:checkbox');
        $checkbox.attr('checked', !$checkbox.attr('checked'));
  	});
+	
+	// refresh token
+	authTokenDiv.addClass('refresh');
+	$('#token-span').click(function(e){
+		renewToken();
+	})
+	
+	
 	
 	/* end of complex form js */
 	

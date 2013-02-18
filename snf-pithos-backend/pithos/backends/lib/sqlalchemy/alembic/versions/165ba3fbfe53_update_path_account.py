507ad8a5f7ca8294ace9a0a01fdf55aaf017fdd0
Sofia Papagiannaki <papagian@gmail.com>
2013-02-18 22:22:31 +0200

    Update pithos migration
    Display progress

--- snf-pithos-backend/pithos/backends/lib/sqlalchemy/alembic/versions/165ba3fbfe53_update_path_account.py
+++ snf-pithos-backend/pithos/backends/lib/sqlalchemy/alembic/versions/165ba3fbfe53_update_path_account.py
@@ -17,6 +17,19 @@ from synnefo.lib.astakos import get_user_uuid, get_displayname as get_user_displ
 from pithos.api.settings import (
     SERVICE_TOKEN, USER_CATALOG_URL, AUTHENTICATION_USERS)
 
+try:
+    from progress.bar import IncrementalBar
+except ImportError:
+    class IncrementalBar():
+        def __init__(self, label, max=100):
+            print label
+
+        def next(self):
+            return
+
+        def finish(self):
+            return
+
 USER_CATALOG_URL = USER_CATALOG_URL.replace('user_catalogs', 'service/api/user_catalogs')
 
 import sqlalchemy as sa
@@ -24,13 +37,12 @@ import sqlalchemy as sa
 catalog = {}
 def get_uuid(account):
     global catalog
-    uuid = catalog.get(account, -1)
-    if uuid != -1:
-        return uuid
+    if account in catalog:
+        return catalog[account]
     try:
         catalog[account] = get_user_uuid(
             SERVICE_TOKEN, account, USER_CATALOG_URL, AUTHENTICATION_USERS)
-        print account, catalog[account]
+        print '\n', account, '-->', catalog[account]
     except:
         raise
     else:
@@ -39,13 +51,12 @@ def get_uuid(account):
 inverse_catalog = {}
 def get_displayname(account):
     global inverse_catalog
-    displayname = inverse_catalog.get(account, -1)
-    if displayname != -1:
-        return displayname
+    if account in inverse_catalog:
+        return inverse_catalog[account]
     try:
         inverse_catalog[account] = get_user_displayname(
             SERVICE_TOKEN, account, USER_CATALOG_URL, AUTHENTICATION_USERS)
-        print account, inverse_catalog[account]
+        print '\n', account, '-->', inverse_catalog[account]
     except:
         raise
     else:
@@ -89,60 +100,81 @@ g =  table(
     column('member', sa.String(256))
 )
 
-def upgrade():
+def migrate(callback):
     connection = op.get_bind()
 
     s = sa.select([n.c.node, n.c.path])
     nodes = connection.execute(s).fetchall()
+    bar = IncrementalBar('Migrating node paths...', max=len(nodes))
     for node, path in nodes:
         account, sep, rest = path.partition('/')
-        uuid = get_uuid(account)
-        if not uuid:
+        match = callback(account)
+        if not match:
+            bar.next()
             continue
-        path = sep.join([uuid, rest])
+        path = sep.join([match, rest])
         u = n.update().where(n.c.node == node).values({'path':path})
         connection.execute(u)
+        bar.next()
+    bar.finish()
 
     s = sa.select([v.c.node, v.c.muser])
     versions = connection.execute(s).fetchall()
+    bar = IncrementalBar('Migrating version modification users...',
+                         max=len(versions)
+    )
     for node, muser in versions:
-        uuid = get_uuid(muser)
-        if not uuid:
+        match = callback(muser)
+        if not match:
+            bar.next()
             continue
-        u = v.update().where(v.c.node == node).values({'muser':uuid})
+        u = v.update().where(v.c.node == node).values({'muser':match})
         connection.execute(u)
+        bar.next()
+    bar.finish()
 
     s = sa.select([p.c.public_id, p.c.path])
     public = connection.execute(s).fetchall()
+    bar = IncrementalBar('Migrating public paths...', max=len(public))
     for id, path in public:
         account, sep, rest = path.partition('/')
-        uuid = get_uuid(account)
-        if not uuid:
+        match = callback(account)
+        if not match:
+            bar.next()
             continue
-        path = sep.join([uuid, rest])
+        path = sep.join([match, rest])
         u = p.update().where(p.c.public_id == id).values({'path':path})
         connection.execute(u)
+        bar.next()
+    bar.finish()
 
     s = sa.select([x.c.feature_id, x.c.path])
     xfeatures = connection.execute(s).fetchall()
+    bar = IncrementalBar('Migrating permission paths...', max=len(xfeatures))
     for id, path in xfeatures:
         account, sep, rest = path.partition('/')
-        uuid = get_uuid(account)
-        if not uuid:
+        match = callback(account)
+        if not match:
+            bar.next()
             continue
-        path = sep.join([uuid, rest])
+        path = sep.join([match, rest])
         u = x.update().where(x.c.feature_id == id).values({'path':path})
         connection.execute(u)
+        bar.next()
+    bar.finish()
 
     s = sa.select([xvals.c.feature_id, xvals.c.key, xvals.c.value])
     s = s.where(xvals.c.value != '*')
     xfeaturevals = connection.execute(s).fetchall()
+    bar = IncrementalBar('Migrating permission holders...',
+                         max=len(xfeaturevals))
     for feature_id, key, value in xfeaturevals:
         account, sep, group = value.partition(':')
-        uuid = get_uuid(account)
-        if not uuid:
+        match = callback(account)
+        if not match:
+            bar.next()
             continue
-        new_value = sep.join([uuid, group])
+        new_value = sep.join([match, group])
         u = xvals.update()
         u = u.where(and_(
                 xvals.c.feature_id == feature_id,
@@ -150,103 +182,34 @@ def upgrade():
                 xvals.c.value == value))
         u = u.values({'value':new_value})
         connection.execute(u)
+        bar.next()
+    bar.finish()
 
     s = sa.select([g.c.owner, g.c.name, g.c.member])
     groups = connection.execute(s).fetchall()
+    bar = IncrementalBar('Migrating group owners & members...',
+                         max=len(groups))
     for owner, name, member in groups:
-        owner_uuid = get_uuid(owner)
-        member_uuid = get_uuid(member)
-        if owner_uuid or member_uuid:
+        owner_match = callback(owner)
+        member_match = callback(member)
+        if owner_match or member_match:
             u = g.update()
             u = u.where(and_(
                 g.c.owner == owner,
                 g.c.name == name,
                 g.c.member == member))
             values = {}
-            if owner_uuid:
-                values['owner'] = owner_uuid
-            if member_uuid:
-                values['member'] = member_uuid
+            if owner_match:
+                values['owner'] = owner_match
+            if member_match:
+                values['member'] = member_match
             u = u.values(values)
             connection.execute(u)
+            bar.next()
+    bar.finish()
+
+def upgrade():
+    migrate(get_uuid)
 
 def downgrade():
-    connection = op.get_bind()
-
-    s = sa.select([n.c.node, n.c.path])
-    nodes = connection.execute(s).fetchall()
-    for node, path in nodes:
-        account, sep, rest = path.partition('/')
-        displayname = get_displayname(account)
-        if not displayname:
-            continue
-        path = sep.join([displayname, rest])
-        u = n.update().where(n.c.node == node).values({'path':path})
-        connection.execute(u)
-
-    s = sa.select([v.c.node, v.c.muser])
-    versions = connection.execute(s).fetchall()
-    for node, muser in versions:
-        displayname = get_displayname(muser)
-        if not displayname:
-            continue
-        u = v.update().where(v.c.node == node).values({'muser':displayname})
-        connection.execute(u)
-
-    s = sa.select([p.c.public_id, p.c.path])
-    public = connection.execute(s).fetchall()
-    for id, path in public:
-        account, sep, rest = path.partition('/')
-        displayname = get_displayname(account)
-        if not displayname:
-            continue
-        path = sep.join([displayname, rest])
-        u = p.update().where(p.c.public_id == id).values({'path':path})
-        connection.execute(u)
-
-    s = sa.select([x.c.feature_id, x.c.path])
-    xfeatures = connection.execute(s).fetchall()
-    for id, path in xfeatures:
-        account, sep, rest = path.partition('/')
-        displayname = get_displayname(account)
-        if not displayname:
-            continue
-        path = sep.join([displayname, rest])
-        u = x.update().where(x.c.feature_id == id).values({'path':path})
-        connection.execute(u)
-
-    s = sa.select([xvals.c.feature_id, xvals.c.key, xvals.c.value])
-    s = s.where(xvals.c.value != '*')
-    xfeaturevals = connection.execute(s).fetchall()
-    for feature_id, key, value in xfeaturevals:
-        account, sep, group = value.partition(':')
-        displayname = get_displayname(account)
-        if not displayname:
-            continue
-        new_value = sep.join([displayname, group])
-        u = xvals.update()
-        u = u.where(and_(
-                xvals.c.feature_id == feature_id,
-                xvals.c.key == key,
-                xvals.c.value ==value))
-        u = u.values({'value':new_value})
-        connection.execute(u)
-
-    s = sa.select([g.c.owner, g.c.name, g.c.member])
-    groups = connection.execute(s).fetchall()
-    for owner, name, member in groups:
-        owner_displayname = get_displayname(owner)
-        member_displayname = get_displayname(member)
-        if owner_displayname or member_displayname:
-            u = g.update()
-            u = u.where(and_(
-                g.c.owner == owner,
-                g.c.name == name,
-                g.c.member == member))
-            values = {}
-            if owner_displayname:
-                values['owner'] = owner_displayname
-            if member_displayname:
-                values['member'] = member_displayname
-            u = u.values(values)
-            connection.execute(u)
+    migrate(get_displayname)

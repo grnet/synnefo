92754615eaee359ce4d9c6edda661a1031eeddd8
Constantinos Venetsanopoulos <cven@grnet.gr>
2013-03-29 13:16:31 +0200

    Update setup.py for every snf-* component
     * Update short descriptions
     * Make sure all emails point to synnefo-devel@googlegroups.com
     * Make sure the author and maintainer is "Synnefo development team"
     * Change pypi URL to point to www.synnefo.org/packages/pypi
     * Extend Copyright for 2012, 2013 wherever needed

--- snf-cyclades-gtools/setup.py
+++ snf-cyclades-gtools/setup.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# Copyright 2011 GRNET S.A. All rights reserved.
+# Copyright 2011, 2012, 2013 GRNET S.A. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following
@@ -44,7 +43,7 @@ from synnefo.versions.ganeti import __version__
 setup(
     name="snf-cyclades-gtools",
     version=__version__,
-    description="Synnefo Ganeti supplementary tools",
+    description="Synnefo tools for interaction with Ganeti",
 
     url="http://www.synnefo.org/",
     author='Synnefo development team',
@@ -55,7 +54,7 @@ setup(
     license="BSD",
     namespace_packages=["synnefo", "synnefo.versions"],
     packages=["synnefo", "synnefo.ganeti", "synnefo.versions"],
-    dependency_links=['http://docs.dev.grnet.gr/pypi'],
+    dependency_links=['http://www.synnefo.org/packages/pypi'],
     install_requires=[
         'snf-common',
         'python-daemon>=1.5.5',

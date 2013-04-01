69b74a54993c49de19a763ffbc484ac992aebe50
Kostas Papadimitriou <kpap@grnet.gr>
2013-04-01 19:14:07 +0300

    Include astakos app in ci/tests.sh

--- ci/tests.sh
+++ ci/tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TEST="$(which snf-manage) test api db logic plankton vmapi --settings=synnefo.settings.test"
+TEST="$(which snf-manage) test api db logic plankton vmapi im --settings=synnefo.settings.test"
 
 if coverage >/dev/null 2>&1; then
   coverage run $TEST

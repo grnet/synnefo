09c39976b0d5f3bada3f8592aec3e1b3e4431868
Kostas Papadimitriou <kpap@grnet.gr>
2013-04-17 19:21:16 +0300

    Astakos missing Service model migrations

--- snf-astakos-app/astakos/im/models.py
+++ snf-astakos-app/astakos/im/models.py
@@ -122,7 +122,7 @@ def dict_merge(a, b):
 class Service(models.Model):
     name = models.CharField(_('Name'), max_length=255, unique=True,
                             db_index=True)
-    api_url = models.CharField(_('Service API url'), max_length=255)
+    api_url = models.CharField(_('Service API url'), max_length=255, null=True)
     auth_token = models.CharField(_('Authentication Token'), max_length=32,
                                   null=True, blank=True)
     auth_token_created = models.DateTimeField(_('Token creation date'),

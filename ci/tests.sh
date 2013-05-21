dcc7f7276a4588208472fb44fe9c2e1477b19d1c
Christos Stavrakakis <cstavr@grnet.gr>
2013-05-21 13:42:57 +0300

    ci: Add failfast option to tests

--- ci/tests.sh
+++ ci/tests.sh
@@ -5,7 +5,7 @@ set -e
 export SYNNEFO_SETTINGS_DIR='/tmp/snf-test-settings'
 
 APPS="api db logic plankton quotas vmapi im quotaholder_app helpdesk"
-TEST="$(which snf-manage) test $APPS --settings=synnefo.settings.test"
+TEST="$(which snf-manage) test $APPS --failfast --traceback --settings=synnefo.settings.test"
 
 if coverage >/dev/null 2>&1; then
   coverage run $TEST

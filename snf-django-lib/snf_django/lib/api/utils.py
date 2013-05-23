810ce4a99e7038cd053c5f39e807e231723ad69f
Christos Stavrakakis <cstavr@grnet.gr>
2013-05-23 17:10:09 +0300

    snf-django: BadRequest if no content-type
    Raise a BadRequest if request is missing Content-Type header field.

--- snf-django-lib/snf_django/lib/api/utils.py
+++ snf-django-lib/snf_django/lib/api/utils.py
@@ -96,6 +96,8 @@ def get_request_dict(request):
     """
     data = request.raw_post_data
     content_type = request.META.get("CONTENT_TYPE")
+    if content_type is None:
+        faults.BadRequest("Missing Content-Type header field")
     if content_type.startswith("application/json"):
         try:
             return json.loads(data)

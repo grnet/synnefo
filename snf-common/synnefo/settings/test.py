55dc9cb4e3e0b1ad328257c9ee08af5adb126ba6
Georgios D. Tsoukalas <gtsouk@grnet.gr>
2013-06-11 15:52:56 +0300

    common: do not overwrite test database setting

--- snf-common/synnefo/settings/test.py
+++ snf-common/synnefo/settings/test.py
@@ -6,12 +6,15 @@ from synnefo.settings import *
 DEBUG = False
 TEST = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'sqlite3',
-        'NAME': '/tmp/synnefo_test_db.sqlite',
+try:
+    DATABASES['default']
+except (NameError, KeyError):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'sqlite3',
+            'NAME': '/tmp/synnefo_test_db.sqlite',
+        }
     }
-}
 
 LOGGING_SETUP['handlers']['console']['level'] = \
     os.environ.get('SYNNEFO_TESTS_LOGGING_LEVEL', 'WARNING')

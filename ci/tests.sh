c222778ea33bbfecbb0c9b151b1c87a7bafef86d
Christos Stavrakakis <cstavr@grnet.gr>
2013-06-17 17:55:12 +0300

    ci: Remove failfast option from tests

--- ci/tests.sh
+++ ci/tests.sh
@@ -7,7 +7,7 @@ SNF_MANAGE=$(which snf-manage) ||
 	{ echo "Cannot find snf-manage in $PATH" 1>&2; exit 1; }
 
 APPS="api db logic plankton quotas vmapi im quotaholder_app helpdesk"
-TEST="$SNF_MANAGE test $APPS --traceback --settings=synnefo.settings.test"
+TEST="$(which snf-manage) test $APPS --traceback --settings=synnefo.settings.test"
 
 if coverage >/dev/null 2>&1; then
   coverage run $TEST

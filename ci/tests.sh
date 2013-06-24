2b20001eb328c9b3df871781d26707b9317ad062
Vangelis Koukis <vkoukis@grnet.gr>
2013-06-25 02:35:39 +0300

    Report failure to find snf-manage in test runner

--- ci/tests.sh
+++ ci/tests.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-export SYNNEFO_SETTINGS_DIR='/tmp/snf-test-settings'
+export SYNNEFO_SETTINGS_DIR=/tmp/snf-test-settings
+
+SNF_MANAGE=$(which snf-manage) ||
+	echo "Cannot find snf-manage in $PATH" 1>&2 && exit 1
 
 APPS="api db logic plankton quotas vmapi im quotaholder_app helpdesk"
-TEST="$(which snf-manage) test $APPS --traceback --settings=synnefo.settings.test"
+TEST="$SNF_MANAGE test $APPS --traceback --settings=synnefo.settings.test"
 
 if coverage >/dev/null 2>&1; then
   coverage run $TEST
   coverage report --include=snf-*
 else
-  echo "coverage not installed"
+  echo "WARNING: Cannot find coverage in path, skipping coverage tests" 1>&2
   $TEST
 fi

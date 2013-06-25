8044568ef06639f0a8d9c8afeaf8c43435a91d76
Vangelis Koukis <vkoukis@grnet.gr>
2013-06-25 03:45:08 +0300

    Fix check for snf-manage in ci/tests.sh

--- ci/tests.sh
+++ ci/tests.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
 set -e
 
 export SYNNEFO_SETTINGS_DIR=/tmp/snf-test-settings
 
 SNF_MANAGE=$(which snf-manage) ||
-	echo "Cannot find snf-manage in $PATH" 1>&2 && exit 1
+	{ echo "Cannot find snf-manage in $PATH" 1>&2; exit 1; }
 
 APPS="api db logic plankton quotas vmapi im quotaholder_app helpdesk"
 TEST="$SNF_MANAGE test $APPS --traceback --settings=synnefo.settings.test"

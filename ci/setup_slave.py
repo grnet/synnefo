03cba903ac4657f5bbde1610502b346f3ca75d12
Christos Stavrakakis <cstavr@grnet.gr>
2013-07-31 14:25:39 +0300

    ci: Get credentials by kamakirc
    Get credentials for deployment from .kamakirc instead of config file.
    Also, add option in config file indicating which cloud for kamaki to
    use. Finally, add option in SynnefoCI to override the cloud from config.
    
    Refs #4193

--- ci/setup_slave.py
+++ ci/setup_slave.py
@@ -10,12 +10,6 @@ from utils import SynnefoCI
 def setup_slave():
     """Setup slave server"""
     synnefo_ci = SynnefoCI(cleanup_config=True)
-    # Get token from /nfs/token
-    try:
-        token = open("/nfs/synnefo_token").read().strip()
-        synnefo_ci.write_config('token', token, 'Deployment')
-    except:
-        pass
     # Build slave server
     synnefo_ci.create_server()
     # Copy synnefo repo to server

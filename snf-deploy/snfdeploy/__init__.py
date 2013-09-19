e9c7a07828eaf1a6a78fd0c1d4e8aecdd3e64b9e
Ilias Tsitsimpis <iliastsi@grnet.gr>
2013-09-19 15:41:16 +0300

    snf-deploy: Remove ssh keys
    Remove ssh keys from snf-deploy package.
    We can now generate new keys running `snf-deploy keygen'.

--- snf-deploy/snfdeploy/__init__.py
+++ snf-deploy/snfdeploy/__init__.py
@@ -16,6 +16,14 @@ from snfdeploy.lib import *
 
 def print_available_actions(command):
 
+  if command == "keygen":
+    print """
+Usage: snf-deploy keygen [--force]
+
+  Generate new ssh keys (both rsa and dsa keypairs)
+
+  """
+
   if command == "vcluster":
     print """
 Usage: snf-deploy vcluster
@@ -372,9 +380,9 @@ def parse_options():
   parser.add_argument("--vnc", dest="vnc",
                       default=False, action="store_true",
                       help="Wheter virtual nodes will have a vnc console or not")
-  parser.add_argument("-k", "--keygen", dest="keygen",
+  parser.add_argument("--force", dest="force",
                       default=False, action="store_true",
-                      help="Whether to create new ssh key pairs")
+                      help="Force the creation of new ssh key pairs")
 
   parser.add_argument("-i", "--ssh-key", dest="ssh_key",
                       default=None,
@@ -399,7 +407,7 @@ def parse_options():
                       choices=["packages", "vcluster", "prepare",
                                "synnefo", "backend", "ganeti",
                                "run", "cleanup", "test",
-                               "all", "add"],
+                               "all", "add", "keygen"],
                       help="Run on of the supported deployment commands")
 
   # available actions for the run command
@@ -487,7 +495,28 @@ def get_actions(*args):
     return ret
 
 
-def create_keys(args, env):
+def must_create_keys(force, env):
+    """Check if we need to create ssh keys
+
+    If force is true we are going to overide the old keys.
+    Else if there are already generated keys to use, don't create new ones.
+
+    """
+    if force:
+        return True
+    d = os.path.join(env.templates, "root/.ssh")
+    auth_keys_exists = os.path.exists(os.path.join(d, "authorized_keys"))
+    dsa_exists = os.path.exists(os.path.join(d, "id_dsa"))
+    dsa_pub_exists = os.path.exists(os.path.join(d, "id_dsa.pub"))
+    rsa_exists = os.path.exists(os.path.join(d, "id_rsa"))
+    rsa_pub_exists = os.path.exists(os.path.join(d, "id_rsa.pub"))
+    # If any of the above doesn't exist return True
+    return not (dsa_exists and dsa_pub_exists
+                and rsa_exists and rsa_pub_exists
+                and auth_keys_exists)
+
+
+def do_create_keys(args, env):
   d = os.path.join(env.templates, "root/.ssh")
   a = os.path.join(d, "authorized_keys")
   for t in ("dsa", "rsa"):
@@ -541,14 +570,25 @@ def main():
   create_dir(env.run, False)
   create_dir(env.dns, False)
 
+  # Check if there are keys to use
+  if args.command == "keygen":
+    if must_create_keys(args.force, env):
+      do_create_keys(args, env)
+      return 0
+    else:
+      print "Keys already existed.. aborting"
+      return 1
+  else:
+    if (args.key_inject and (args.ssh_key is None)
+        and must_create_keys(False, env)):
+      print "No ssh keys to use. Run `snf-deploy keygen' first."
+      return 1
+
   if args.command == "test":
     conf.print_config()
 
   if args.command == "cleanup":
     cleanup(args, env)
-
-  if args.keygen:
-    create_keys(args, env)
 
   if args.command == "packages":
     create_dir(env.packages, True)

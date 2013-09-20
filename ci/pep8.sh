78c77ddd2c690df063358d780dc4e3c387768e74
Ilias Tsitsimpis <iliastsi@grnet.gr>
2013-09-20 15:16:43 +0300

    ci: Check only .py files for pep8 errors

--- ci/pep8.sh
+++ ci/pep8.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # Files to be excluded from pep8 tests
 EXCLUDE=migrations,build,setup,distribute_setup.py,\
 setup.py,rapi.py,dictconfig.py,ordereddict.py
 
-if [ $# -eq 0 ]; then
+# Keep only *.py files
+py_files=`echo "$@" | awk '/.*\.py/' RS=" "`
+
+if [ -z "$py_files" ]; then
     echo "No files to be tested"
 else
-    pep8 --exclude=$EXCLUDE "$@"
+    pep8 --exclude=$EXCLUDE "$py_files"
 fi

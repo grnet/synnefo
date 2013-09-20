909a612d5bd3063e2760f96f6d284f2210f2fea4
Ilias Tsitsimpis <iliastsi@grnet.gr>
2013-09-20 14:25:25 +0300

    ci: Exit if there are no file to check with pep8

--- ci/pep8.sh
+++ ci/pep8.sh
@@ -4,4 +4,8 @@
 EXCLUDE=migrations,build,setup,distribute_setup.py,\
 setup.py,rapi.py,dictconfig.py,ordereddict.py
 
-pep8 --exclude=$EXCLUDE "$@"
+if [ $# -eq 0 ]; then
+    echo "No files to be tested"
+else
+    pep8 --exclude=$EXCLUDE "$@"
+fi

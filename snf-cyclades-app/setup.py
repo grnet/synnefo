28ceb931e572b447f47e2b2fac47e0dd4baece7f
Christos Stavrakakis <cstavr@grnet.gr>
2013-09-24 13:02:51 +0300

    wheezy: Update Django dependency to <=1.4.5

--- snf-cyclades-app/setup.py
+++ snf-cyclades-app/setup.py
@@ -57,7 +57,7 @@ CLASSIFIERS = []
 
 # Package requirements
 INSTALL_REQUIRES = [
-    'Django >=1.2, <1.3',
+    'Django >=1.2, <=1.4.5',
     'simplejson>=2.1.1',
     'pycurl>=7.19.0',
     'python-dateutil>=1.4.1',
--- snf-pithos-app/setup.py
+++ snf-pithos-app/setup.py
@@ -59,7 +59,7 @@ CLASSIFIERS = []
 INSTALL_REQUIRES = [
     'snf-common',
     'snf-pithos-backend',
-    'Django>=1.2, <1.3',
+    'Django>=1.2, <=1.4.5',
     'objpool>=0.2',
     'astakosclient',
     'snf-django-lib',

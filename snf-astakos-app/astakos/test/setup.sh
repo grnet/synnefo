13419268f4fa7c162c3779ac6c65b29fa3f053f9
Giorgos Korfiatis <gkorf@grnet.gr>
2013-10-18 15:44:27 +0300

    astakos: Fix system setup in stress test

--- snf-astakos-app/astakos/test/setup.sh
+++ snf-astakos-app/astakos/test/setup.sh
@@ -4,3 +4,14 @@ d=`dirname $0`
 export SYNNEFO_SETTINGS_DIR=$d/settings
 snf-manage syncdb --noinput
 snf-manage migrate
+
+HOST=127.0.0.1:8000
+
+snf-manage component-add astakos --base-url $HOST/astakos
+snf-manage component-add cyclades --base-url $HOST/cyclades
+snf-manage component-add pithos --base-url $HOST/pithos
+
+snf-service-export astakos $HOST/astakos | snf-manage service-import --json -
+snf-service-export cyclades $HOST/cyclades | snf-manage service-import --json -
+snf-service-export pithos $HOST/pithos | snf-manage service-import --json -
+snf-manage resource-modify astakos.pending_app --limit 2

a821743fac075f5380249ab624d989e46702bf9b
Kostas Papadimitriou <kpap@grnet.gr>
2013-10-30 15:36:26 +0200

    webproject: Do not require django.contrib.admin by default

--- snf-webproject/synnefo/webproject/settings/__init__.py
+++ snf-webproject/synnefo/webproject/settings/__init__.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = (
     #'django.contrib.sessions',
     'django.contrib.sites',
     #'django.contrib.messages',
-    'django.contrib.admin',
     'south',
     'synnefo.webproject'
 )

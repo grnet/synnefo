bfe237a24c9d8fff5f596d6224421732b9f7e7ac
Sofia Papagiannaki <spapagian@grnet.gr>
2013-11-19 17:23:02 +0200

    astakos: Introduce CRUD management commands for oa2 clients

---
+++ snf-astakos-app/astakos/oa2/management/commands/oa2-client-add.py
@@ -0,0 +1,92 @@
+# Copyright 2013 GRNET S.A. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or
+# without modification, are permitted provided that the following
+# conditions are met:
+#
+#   1. Redistributions of source code must retain the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer.
+#
+#   2. Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials
+#      provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY GRNET S.A. ``AS IS'' AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GRNET S.A OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and
+# documentation are those of the authors and should not be
+# interpreted as representing official policies, either expressed
+# or implied, of GRNET S.A.
+
+from optparse import make_option
+
+from django.db import transaction
+from django.core.management.base import CommandError
+
+from snf_django.management.commands import SynnefoCommand
+
+from astakos.oa2.models import Client, RedirectUrl
+
+
+class Command(SynnefoCommand):
+    args = "<identfier>"
+    help = "Create a oauth2 client"
+
+    option_list = SynnefoCommand.option_list + (
+        make_option('--secret',
+                    dest='secret',
+                    metavar='SECRET',
+                    action='store',
+                    default=None,
+                    help="Set client's secret"),
+        make_option('--is-trusted',
+                    action='store_true',
+                    dest='is_trusted',
+                    default=False,
+                    help="Whether client is trusted or not"),
+        make_option('--type',
+                    action='store',
+                    dest='type',
+                    default='confidential',
+                    help="Set client's type"),
+        make_option('--url',
+                    action='append',
+                    dest='urls',
+                    default=[],
+                    help="Set client's redirect URLs"),
+    )
+
+    @transaction.commit_on_success
+    def handle(self, *args, **options):
+        if len(args) != 1:
+            raise CommandError("Invalid number of arguments")
+
+        identifier = args[0].decode('utf8')
+
+        try:
+            c = Client(identifier=identifier, secret=options['secret'],
+                       type=options['type'], is_trusted=options['is_trusted'])
+            c.save()
+            c.redirecturl_set.bulk_create((RedirectUrl(client=c, url=url) for
+                                          url in options['urls']))
+            c.save()
+
+        except BaseException, e:
+            import traceback
+            traceback.print_exc()
+            raise CommandError(e)
+        else:
+            self.stdout.write('Client created successfully\n')

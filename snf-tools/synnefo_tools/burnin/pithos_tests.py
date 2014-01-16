db4d4b8d0b951f82f9c55350803496ea74309e17
Stavros Sachtouris <saxtouri@admin.grnet.gr>
2014-01-16 18:48:35 +0200

    burnin: Add pithos account_head/get tests
    Reviewed-by: Ilias Tsitsimpis <iliastsi@grnet.gr>

--- snf-tools/synnefo_tools/burnin/pithos_tests.py
+++ snf-tools/synnefo_tools/burnin/pithos_tests.py
@@ -21,6 +21,7 @@ This is the burnin class that tests the Pithos functionality
 import os
 import random
 import tempfile
+from datetime import datetime
 
 from synnefo_tools.burnin.common import BurninTests, Proper, \
     QPITHOS, QADD, QREMOVE
@@ -31,20 +32,96 @@ class PithosTestSuite(BurninTests):
     """Test Pithos functionality"""
     containers = Proper(value=None)
     created_container = Proper(value=None)
+    now_unformated = Proper(value=datetime.utcnow())
 
-    def test_001_list_containers(self):
-        """Test container list actually returns containers"""
+    def test_005_account_head(self):
+        """HEAD on pithos account"""
         self._set_pithos_account(self._get_uuid())
+        pithos = self.clients.pithos
+        resp = pithos.account_head()
+        self.assertEqual(resp.status_code, 204)
+        self.info('Returns 204')
+
+        resp = pithos.account_head(until='1000000000')
+        self.assertEqual(resp.status_code, 204)
+        datestring = unicode(resp.headers['x-account-until-timestamp'])
+        self.assertEqual(u'Sun, 09 Sep 2001 01:46:40 GMT', datestring)
+        self.assertTrue(any([
+            h.startswith('x-account-policy-quota') for h in resp.headers]))
+        self.info('Until and account policy quota exist')
+
+        for date_format in pithos.DATE_FORMATS:
+            now_formated = self.now_unformated.strftime(date_format)
+            resp1 = pithos.account_head(
+                if_modified_since=now_formated, success=(204, 304, 412))
+            resp2 = pithos.account_head(
+                if_unmodified_since=now_formated, success=(204, 304, 412))
+            self.assertNotEqual(resp1.status_code, resp2.status_code)
+        self.info('If_(un)modified_since is OK')
+
+    def test_010_account_get(self):  # pylint: disable=too-many-locals
+        """Test account_get"""
+        self.info('Preparation')
+        pithos = self.clients.pithos
+        for i in range(1, 3):
+            cont_name = "cont%s_%s%s" % (
+                i, self.run_id, random.randint(1000, 9999))
+            self._create_pithos_container(cont_name)
+        pithos.container, obj = cont_name, 'shared_file'
+        pithos.create_object(obj)
+        pithos.set_object_sharing(obj, read_permission='*')
+        self.info('Created object /%s/%s' % (cont_name, obj))
+
+        resp = pithos.list_containers()
+        full_len = len(resp)
+        self.assertTrue(full_len > 2)
+        self.info('Normal use is OK')
+
+        resp = pithos.account_get(limit=1)
+        self.assertEqual(len(resp.json), 1)
+        self.info('Limit works')
+
+        resp = pithos.account_get(marker='cont')
+        cont1, cont3 = resp.json[0], resp.json[2]
+        self.info('Marker works')
+
+        resp = pithos.account_get(limit=2, marker='cont')
+        conames = [container['name'] for container in resp.json if (
+            container['name'].lower().startswith('cont'))]
+        self.assertTrue(cont1['name'] in conames)
+        self.assertFalse(cont3['name'] in conames)
+        self.info('Marker-limit combination works')
+
+        resp = pithos.account_get(show_only_shared=True)
+        self.assertTrue(cont_name in [c['name'] for c in resp.json])
+        self.info('Show-only-shared works')
+
+        resp = pithos.account_get(until=1342609206.0)
+        self.assertTrue(len(resp.json) <= full_len)
+        self.info('Until works')
+
+        for date_format in pithos.DATE_FORMATS:
+            now_formated = self.now_unformated.strftime(date_format)
+            resp1 = pithos.account_get(
+                if_modified_since=now_formated, success=(200, 304, 412))
+            resp2 = pithos.account_get(
+                if_unmodified_since=now_formated, success=(200, 304, 412))
+            self.assertNotEqual(resp1.status_code, resp2.status_code)
+        self.info('If_(un)modified_since is OK')
+
+    def test_051_list_containers(self):
+        """Test container list actually returns containers"""
+        # self.assertTrue(False)   # This is a stoper!
         self.containers = self._get_list_of_containers()
         self.assertGreater(len(self.containers), 0)
 
-    def test_002_unique_containers(self):
+    def test_052_unique_containers(self):
         """Test if containers have unique names"""
         names = [n['name'] for n in self.containers]
         names = sorted(names)
         self.assertEqual(sorted(list(set(names))), names)
 
-    def test_003_create_container(self):
+    def test_053_create_container(self):
         """Test creating a new container"""
         names = [n['name'] for n in self.containers]
         while True:
@@ -65,7 +142,7 @@ class PithosTestSuite(BurninTests):
         # at cleanup phase, if something goes wrong.
         self.created_container = rand_name
 
-    def test_004_upload_file(self):
+    def test_054_upload_file(self):
         """Test uploading a txt file to Pithos"""
         # Create a tmp file
         with tempfile.TemporaryFile(dir=self.temp_directory) as fout:
@@ -80,7 +157,7 @@ class PithosTestSuite(BurninTests):
                 {self._get_uuid(): [(QPITHOS, QADD, size, None)]}
             self._check_quotas(changes)
 
-    def test_005_download_file(self):
+    def test_055_download_file(self):
         """Test downloading the file from Pithos"""
         # Create a tmp directory to save the file
         with tempfile.TemporaryFile(dir=self.temp_directory) as fout:
@@ -92,7 +169,7 @@ class PithosTestSuite(BurninTests):
             self.info("Comparing contents with the uploaded file")
             self.assertEqual(contents, "This is a temp file")
 
-    def test_006_remove(self):
+    def test_056_remove(self):
         """Test removing files and containers from Pithos"""
         self.info("Removing the file %s from container %s",
                   "test.txt", self.created_container)

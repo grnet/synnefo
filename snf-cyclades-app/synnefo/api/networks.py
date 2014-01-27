85aa8c82aa0ac9339aeb858942fb13f816cfa513
Dionysis Grigoropoulos <dgrig@grnet.gr>
2014-01-27 15:32:57 +0200

    cyclades: Typo fix in api/networks.py
    * Fix a typo that made responses report all networks as "deleted=True"

--- snf-cyclades-app/synnefo/api/networks.py
+++ snf-cyclades-app/synnefo/api/networks.py
@@ -191,7 +191,7 @@ def network_to_dict(network, detail=True):
         d['admin_state_up'] = True
         d['subnets'] = subnet_ids
         d['SNF:floating_ip_pool'] = network.floating_ip_pool
-        d['deleted'] = True
+        d['deleted'] = network.deleted
     return d
 
 

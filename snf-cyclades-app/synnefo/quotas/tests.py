0e5605298f0ead2ed792cb731c42f7338737ae8c
Giorgos Korfiatis <gkorf@grnet.gr>
2014-02-28 13:00:48 +0200

    Update Copyright for project-related changes
    Update Copyright for files merged into develop in 2014 concerning the new
    projects mechanism.

--- snf-cyclades-app/synnefo/quotas/tests.py
+++ snf-cyclades-app/synnefo/quotas/tests.py
@@ -2,7 +2,7 @@
 #
 # -*- coding: utf-8 -*-
 #
-# Copyright 2013 GRNET S.A. All rights reserved.
+# Copyright 2013-2014 GRNET S.A. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following

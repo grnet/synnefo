22e9bcc01e58e2b79e117ce8f9c6eb8f17c3dfb5
Christos Stavrakakis <cstavr@grnet.gr>
2014-03-19 11:47:24 +0200

    Update copyright to changed files

--- snf-pithos-app/pithos/api/test/__init__.py
+++ snf-pithos-app/pithos/api/test/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #coding=utf8
-
-# Copyright 2011-2013 GRNET S.A. All rights reserved.
+# Copyright 2011-2014 GRNET S.A. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or
 # without modification, are permitted provided that the following

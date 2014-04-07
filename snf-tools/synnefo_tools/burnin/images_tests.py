8e5403b48a4fd5c6cd3899e694a72b73792c2aeb
Ilias Tsitsimpis <iliastsi@grnet.gr>
2014-04-07 15:21:07 +0300

    burnin: Fix a bug when uploading an image
    Use 'burnin-images' container when uploading/registering an image.

--- snf-tools/synnefo_tools/burnin/images_tests.py
+++ snf-tools/synnefo_tools/burnin/images_tests.py
@@ -149,6 +149,7 @@ class ImagesTestSuite(BurninTests):
         """Upload the image to Pithos"""
         self._set_pithos_account(self._get_uuid())
         self._create_pithos_container("burnin-images")
+        self._set_pithos_container("burnin-images")
         file_size = os.path.getsize(self.temp_image_file)
         with open(self.temp_image_file, "r+b") as fin:
             self.clients.pithos.upload_object(self.temp_image_name, fin)
@@ -191,6 +192,7 @@ class ImagesTestSuite(BurninTests):
         """Remove uploaded image from Pithos"""
         # Remove uploaded image
         self.info("Deleting uploaded image %s", self.temp_image_name)
+        self._set_pithos_container("burnin-images")
         self.clients.pithos.del_object(self.temp_image_name)
         # Verify quotas
         file_size = os.path.getsize(self.temp_image_file)
@@ -208,6 +210,7 @@ class ImagesTestSuite(BurninTests):
         """Clean up"""
         if cls.temp_image_name is not None:
             try:
+                cls.clients.pithos.container = "burnin-images"
                 cls.clients.pithos.del_object(cls.temp_image_name)
             except ClientError:
                 pass

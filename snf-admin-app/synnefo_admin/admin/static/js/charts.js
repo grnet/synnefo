ec1331cd511457987de8c49285ea7d0d13dee3af
Athina Bekakou <athinabek@grnet.gr>
2014-04-29 20:03:28 +0300

    admin-ui: Add timeline and bar charts

--- snf-admin-app/synnefo_admin/admin/static/js/charts.js
+++ snf-admin-app/synnefo_admin/admin/static/js/charts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	Highcharts.setOptions({colors: ['#00BC8C', '#f45b5b', '#f7a35c', '#858585', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']});
+	Highcharts.setOptions({colors: ['#69cd8e', '#f45b5b', '#f7a35c', '#aaa6a6', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']});
 	// with drilldown
 	$('#pie-drilldown').highcharts({
 	chart: {
@@ -41,7 +41,8 @@ $(document).ready(function() {
 					// sliced: true,
 					// selected: true,
 					num: 3000,
-					drilldown: 'providers-act'
+					drilldown: 'providers-act',
+					// color: '#69cd8e'
 				},
 				{
 					name: 'Inactive',
@@ -69,11 +70,13 @@ $(document).ready(function() {
 					data: [
 						{
 							name: 'Shibboleth',
-							y: 60 // percentage is per parent-slice (60% of active accounts are shibboleth)
+							y: 60, // percentage is per parent-slice (60% of active accounts are shibboleth)
+
 						},
 						{
 							name: 'Local',
-							y: 40
+							y: 40,
+							color: '#839dd6'
 						}]
 				},
 				{
@@ -85,7 +88,8 @@ $(document).ready(function() {
 						},
 						{
 						name: 'Local',
-						y: 90
+						y: 90,
+						color: '#839dd6'
 						}]
 				},
 				{
@@ -97,7 +101,8 @@ $(document).ready(function() {
 						},
 					{
 						name: 'Local',
-						y: 100
+						y: 100,
+						color: '#839dd6'
 					}]
 				},
 				{
@@ -109,7 +114,8 @@ $(document).ready(function() {
 						},
 						{
 							name: 'Local',
-							y: 80
+							y: 80,
+							color: '#839dd6'
 						}]
 				}]
 		}
@@ -204,5 +210,213 @@ $('#pie-simple').highcharts({
 	//     	population: 700,
 	//     	percentage: 0
 	//     }]
+
+
+		$('#timeline1').highcharts('StockChart', {
+			rangeSelector : {
+				selected : 3,
+				inputEnabled: $('#timeline1').width() > 480
+			},
+			yAxis: {
+				// title: {
+				// 	text: 'Activated Users'
+				// },
+			// 	// opposite: true,
+				lineWidth: 1,
+				tickWidth: 1,
+				// offset: 20,
+				labels: {
+					align: 'left',
+					x: 18,
+					y: 3
+				}
+			},
+			// xAxis: [{
+
+			// 	lineWidth: 2,
+			// 	tickWidth: 2
+			// }],
+			title : {
+				text : 'Demo Zoomable Timeline'
+			},
+			series : [{
+				name : 'demo',
+				data : timelineArray,
+				tooltip: {
+					valueDecimals: 2
+				}
+			}]
+		});
+		$('#timeline2').highcharts('StockChart', {
+			rangeSelector : {
+				selected : 0,
+				inputEnabled: $('#timeline2').width() > 480
+			},
+			title : {
+				text : 'Demo Zoomable Timeline'
+			},
+			series : [{
+				name : 'demo',
+				data : timelineArray,
+				marker : {
+					enabled : true,
+					radius : 3
+				},
+				shadow : true,
+				tooltip: {
+					valueDecimals: 2
+				}
+			}]
+		});
+		$('#timeline3').highcharts('StockChart', {
+			rangeSelector : {
+				selected : 4,
+				inputEnabled: $('#timeline3').width() > 480
+			},
+			title : {
+				text : 'Demo Zoomable Timeline'
+			},
+			series : [{
+				name : 'demo',
+				data : timelineArray,
+				type: 'area',
+				threshold : null,
+				// marker : {
+					// enabled : true,
+					// radius : 3
+				// },
+				// shadow : true,
+				tooltip: {
+					valueDecimals: 2
+				},
+				fillColor : {
+					linearGradient : {
+						x1: 0,
+						y1: 0,
+						x2: 0,
+						y2: 1
+					},
+					stops : [
+						[0, Highcharts.getOptions().colors[7]],
+						[1, Highcharts.Color(Highcharts.getOptions().colors[3]).setOpacity(0).get('rgba')]
+					]
+				}
+			}]
+		});
+		$('#timeline4').highcharts({
+            chart: {
+                zoomType: 'xy'
+            },
+            title: {
+                text: 'Demo Alternative Way to Zoom'
+            },
+            subtitle: {
+                text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' :
+                    'Pinch the chart to zoom in'
+            },
+            xAxis: {
+                type: 'datetime',
+                minRange: 14 * 24 * 3600000 // fourteen days
+            },
+            yAxis: {
+                title: {
+                    text: 'Blah Blah'
+                },
+                // opposite: true // here is working just fine
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                name: 'Demo',
+                data: timelineArray
+            }]
+        });
+
+	$('#columnchart').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'VMs per disk'
+            },
+            // subtitle: {
+            //     text: ''
+            // },
+            xAxis: {
+                categories: [
+                    'Jan',
+                    'Feb',
+                    'Mar',
+                    'Apr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Aug',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Dec'
+                ]
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Number of VMs'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0,
+                    pointWidth: 20
+                }
+            },
+            series: [{
+                name: '100GB',
+                data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+            }, {
+                name: '30GB',
+                data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+            }, {
+                name: '20GB',
+                data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+            }, {
+                name: '5GB',
+                data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+            }]
+        });
 
 });

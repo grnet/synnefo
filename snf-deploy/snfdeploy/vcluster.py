db27ebe155a40a846d1c8f3a7c61b31592df4468
Dimitris Aragiorgis <dimara@grnet.gr>
2014-05-15 17:50:15 +0300

    deploy: Fix vcluster wrt disk images
    Remove the `os` setting from vcluster.conf and keep only one
    image URL (which will also be removed in following patches
    and replaced by either a debootstrap process or an official
    debian base image from snf-image repo).
    
    Use truncate instead of dd to create the local file
    used for extra disk in the cluster's VMs.
    
    Signed-off-by: Dimitris Aragiorgis <dimara@grnet.gr>

--- snf-deploy/snfdeploy/vcluster.py
+++ snf-deploy/snfdeploy/vcluster.py
@@ -149,14 +149,11 @@ def network():
 
 
 def image():
-    # FIXME: Create a clean wheezy image and use it for vcluster
-    if config.os == "ubuntu":
-        url = config.ubuntu_image_url
-    else:
-        url = config.squeeze_image_url
+    # FIXME: Create a wheezy image with debootstrap and use it for vcluster
+    url = config.image_url
 
-    disk0 = "{0}/{1}.disk0".format(config.vcluster_dir, config.os)
-    disk1 = "{0}/{1}.disk1".format(config.vcluster_dir, config.os)
+    disk0 = "{0}/vcluster.disk0".format(config.vcluster_dir)
+    disk1 = "{0}/vcluster.disk1".format(config.vcluster_dir)
 
     create_dir(config.vcluster_dir, False)
 
@@ -165,17 +162,18 @@ def image():
         os.system(cmd)
 
     if config.create_extra_disk and not os.path.exists(disk1):
+        disk1_base = os.path.basename(disk1)
         if config.lvg:
             cmd = """
-lvcreate -L30G -n{0}.disk1 {1}
-""".format(config.os, config.lvg)
+lvcreate -L30G -n{0} {1}
+""".format(disk1_base, config.lvg)
             os.system(cmd)
             cmd = """
-ln -s /dev/{0}/{1}.disk1 {2}
-""".format(config.lvg, config.os, disk1)
+ln -s /dev/{0}/{1} {2}
+""".format(config.lvg, disk1_base, disk1)
             os.system(cmd)
         else:
-            cmd = "dd if=/dev/zero of={0} bs=10M count=3000".format(disk1)
+            cmd = "truncate -s 30G {0}".format(disk1)
             os.system(cmd)
 
 
@@ -200,15 +198,15 @@ def _launch_vm(name, mac):
         graphics = "-nographic"
 
     disks = """ \
--drive file={0}/{1}.disk0,format=raw,if=none,id=drive0,snapshot=on \
+-drive file={0}/vcluster.disk0,format=raw,if=none,id=drive0,snapshot=on \
 -device virtio-blk-pci,drive=drive0,id=virtio-blk-pci.0 \
-""".format(config.vcluster_dir, config.os)
+""".format(config.vcluster_dir)
 
     if config.create_extra_disk:
         disks += """ \
--drive file={0}/{1}.disk1,format=raw,if=none,id=drive1,snapshot=on \
+-drive file={0}/vcluster.disk1,format=raw,if=none,id=drive1,snapshot=on \
 -device virtio-blk-pci,drive=drive1,id=virtio-blk-pci.1 \
-""".format(config.vcluster_dir, config.os)
+""".format(config.vcluster_dir)
 
     ifup = config.lib_dir + "/ifup"
     nics = """ \

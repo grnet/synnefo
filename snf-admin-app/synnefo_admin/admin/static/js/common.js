1614544ca7961226151aa31db7f728f35db3a357
Alex Pyrgiotis <apyrgio@grnet.gr>
2014-06-26 17:47:01 +0300

    admin: Show current place in navbar properly
    Fix apyrgio/synnefo#122

--- snf-admin-app/synnefo_admin/admin/static/js/common.js
+++ snf-admin-app/synnefo_admin/admin/static/js/common.js
@@ -58,35 +58,4 @@ $(document).ready(function(){
     e.preventDefault();
   })
   $("[data-toggle=popover]").popover();
-
-  /* Temporary: the server should indicate the current place */
-  var curPath = window.location.pathname;
-
-  function showCurrentPlace() {
-    var pathArray = curPath.split('/');
-
-    $('.main-nav li').each(function () {
-
-      if($(this).find('a').attr('href') === curPath) {
-        if($(this).closest('ul').hasClass('dropdown-menu')) {
-          $(this).closest('li').addClass('active');
-          $(this).closest('ul').closest('li').addClass('active');
-        }
-        else {
-          $(this).closest('li').addClass('active');
-
-        }
-      }
-      else if('/'+pathArray[1]+'/'+pathArray[2] === $(this).find('a').attr('href')) { // sumvasi! ***
-        $(this).closest('li').addClass('active');
-      }
-      else {
-        $(this).closest('li').removeClass('active');
-      }
-    });
-  };
-
-  showCurrentPlace();
-
-
 })

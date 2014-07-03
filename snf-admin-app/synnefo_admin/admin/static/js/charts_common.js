a50d6221f03328f6751b0f83ce9e6cb2e633a007
Alex Pyrgiotis <apyrgio@grnet.gr>
2014-07-03 12:08:52 +0300

    admin: Small fixes before demo

--- snf-admin-app/synnefo_admin/admin/static/js/charts_common.js
+++ snf-admin-app/synnefo_admin/admin/static/js/charts_common.js
@@ -70,6 +70,7 @@ $(document).ready(function() {
 	   colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
 	      "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
 	   chart: {
+          width: 1000,
 	      backgroundColor: {
 	         linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
 	         stops: [

grnet/synnefo

commit db464c6fdd57b1feb46a8b1df81d2cbe81d1b57c

astakos: Use bulk_create to speed up commissions
Update holdings and create provisions and log in bulk when issuing or
resolving commissions to speed up the operation,

snf-astakos-app/astakos/quotaholder_app/callpoint.py
@@ -136,11 +136,13 @@ def issue_commission(clientkey, provisions, name="", force=False):
     provisions = _merge_same_keys(provisions)
     keys = [key for (key, value) in provisions]
     holdings = _get_holdings_for_update(keys)
+    changed_holdings = {}
     try:
         for key, quantity in provisions:
             # Target
             try:
                 th = holdings[key]
+                changed_holdings[th.id] = th
             except KeyError:
                 m = ("There is no such holding %s" % unicode(key))
                 provision = _mkProvision(key, quantity)
@@ -156,20 +158,22 @@ def issue_commission(clientkey, provisions, name="", force=False):
 
             holdings[key] = th
             provisions_to_create.append((key, quantity))
-
     except QuotaholderError:
-        operations.revert()
         raise
 
+    Holding.objects.filter(id__in=changed_holdings.keys()).delete()
+    Holding.objects.bulk_create(changed_holdings.values())
     commission = Commission.objects.create(clientkey=clientkey,
                                            name=name,
                                            issue_datetime=datetime.now())
+    ps = []
     for (holder, source, resource), quantity in provisions_to_create:
-        Provision.objects.create(serial=commission,
-                                 holder=holder,
-                                 source=source,
-                                 resource=resource,
-                                 quantity=quantity)
+        ps.append(Provision(serial=commission,
+                            holder=holder,
+                            source=source,
+                            resource=resource,
+                            quantity=quantity))
+    Provision.objects.bulk_create(ps)
 
     return commission.serial
 
@@ -191,7 +195,7 @@ def _log_provision(commission, provision, holding, log_datetime, reason):
         'reason':              reason,
     }
 
-    ProvisionLog.objects.create(**kwargs)
+    return ProvisionLog(**kwargs)
 
 
 def _get_commissions_for_update(clientkey, serials):
@@ -252,12 +256,17 @@ def resolve_pending_commissions(clientkey, accept_set=None, reject_set=None,
         accepted.append(serial) if accept else rejected.append(serial)
 
         ps = provisions.get(serial, [])
+        changed_holdings = {}
+        provision_ids = []
+        plog = []
         for pv in ps:
             key = pv.holding_key()
             h = holdings.get(key)
+            changed_holdings[h.id] = h
             if h is None:
                 raise CorruptedError("Corrupted provision")
 
+            provision_ids.append(pv.id)
             quantity = pv.quantity
             action = finalize if accept else undo
             if quantity >= 0:
@@ -267,8 +276,12 @@ def resolve_pending_commissions(clientkey, accept_set=None, reject_set=None,
 
             prefix = 'ACCEPT:' if accept else 'REJECT:'
             comm_reason = prefix + reason[-121:]
-            _log_provision(commission, pv, h, log_datetime, comm_reason)
-            pv.delete()
+            plog.append(
+                _log_provision(commission, pv, h, log_datetime, comm_reason))
+        Holding.objects.filter(id__in=changed_holdings.keys()).delete()
+        Holding.objects.bulk_create(changed_holdings.values())
+        Provision.objects.filter(id__in=provision_ids).delete()
+        ProvisionLog.objects.bulk_create(plog)
         commission.delete()
     return accepted, rejected, notFound, conflicting
 

snf-astakos-app/astakos/quotaholder_app/commission.py
@@ -77,12 +77,10 @@ class Import(Operation):
                                   usage=usage_max)
 
         holding.usage_max = new_usage_max
-        holding.save()
 
     @classmethod
     def _finalize(cls, holding, quantity):
         holding.usage_min += quantity
-        holding.save()
 
 
 class Release(Operation):
@@ -104,12 +102,10 @@ class Release(Operation):
                                   usage=usage_min)
 
         holding.usage_min = new_usage_min
-        holding.save()
 
     @classmethod
     def _finalize(cls, holding, quantity):
         holding.usage_max -= quantity
-        holding.save()
 
 
 class Operations(object):

snf-astakos-app/astakos/quotaholder_app/tests.py
@@ -83,6 +83,12 @@ class QuotaholderTest(TestCase):
         with assertRaises(NoCommissionError):
             qh.get_commission(self.client, s1+1)
 
+        r = qh.get_quota()
+        self.assertEqual(r,
+                         {(holder, source, resource1): (limit1, 0, limit1/2),
+                          (holder, source, resource2): (limit2, 0, limit2),
+                          })
+
         # commission exceptions
 
         with assertRaises(NoCapacityError) as cm:

commit 65e3a418831c23edd5ab33b72bac81dd7f93ea24

astakos: Revert bulk update of holdings in commissions
db464c6 introduced a bug: deleting holdings (and then bulk creating them)
interferes with locking; a thread that waits for holdings will not get
the deleted ones.

Revert to separate updating of holdings when issuing and resolving
commissions.

snf-astakos-app/astakos/quotaholder_app/callpoint.py
@@ -136,13 +136,11 @@ def issue_commission(clientkey, provisions, name="", force=False):
     provisions = _merge_same_keys(provisions)
     keys = [key for (key, value) in provisions]
     holdings = _get_holdings_for_update(keys)
-    changed_holdings = {}
     try:
         for key, quantity in provisions:
             # Target
             try:
                 th = holdings[key]
-                changed_holdings[th.id] = th
             except KeyError:
                 m = ("There is no such holding %s" % unicode(key))
                 provision = _mkProvision(key, quantity)
@@ -159,10 +157,9 @@ def issue_commission(clientkey, provisions, name="", force=False):
             holdings[key] = th
             provisions_to_create.append((key, quantity))
     except QuotaholderError:
+        operations.revert()
         raise
 
-    Holding.objects.filter(id__in=changed_holdings.keys()).delete()
-    Holding.objects.bulk_create(changed_holdings.values())
     commission = Commission.objects.create(clientkey=clientkey,
                                            name=name,
                                            issue_datetime=datetime.now())
@@ -256,7 +253,6 @@ def resolve_pending_commissions(clientkey, accept_set=None, reject_set=None,
         accepted.append(serial) if accept else rejected.append(serial)
 
         ps = provisions.get(serial, [])
-        changed_holdings = {}
         provision_ids = []
         plog = []
         for pv in ps:
@@ -264,8 +260,6 @@ def resolve_pending_commissions(clientkey, accept_set=None, reject_set=None,
             h = holdings.get(key)
             if h is None:
                 raise CorruptedError("Corrupted provision '%s'" % key)
-
-            changed_holdings[h.id] = h
 
             provision_ids.append(pv.id)
             quantity = pv.quantity
@@ -279,8 +273,6 @@ def resolve_pending_commissions(clientkey, accept_set=None, reject_set=None,
             comm_reason = prefix + reason[-121:]
             plog.append(
                 _log_provision(commission, pv, h, log_datetime, comm_reason))
-        Holding.objects.filter(id__in=changed_holdings.keys()).delete()
-        Holding.objects.bulk_create(changed_holdings.values())
         Provision.objects.filter(id__in=provision_ids).delete()
         ProvisionLog.objects.bulk_create(plog)
         commission.delete()

snf-astakos-app/astakos/quotaholder_app/commission.py
@@ -77,10 +77,12 @@ class Import(Operation):
                                   usage=usage_max)
 
         holding.usage_max = new_usage_max
+        holding.save()
 
     @classmethod
     def _finalize(cls, holding, quantity):
         holding.usage_min += quantity
+        holding.save()
 
 
 class Release(Operation):
@@ -102,10 +104,12 @@ class Release(Operation):
                                   usage=usage_min)
 
         holding.usage_min = new_usage_min
+        holding.save()
 
     @classmethod
     def _finalize(cls, holding, quantity):
         holding.usage_max -= quantity
+        holding.save()
 
 
 class Operations(object):

snf-astakos-app/astakos/quotaholder_app/tests.py
@@ -138,6 +138,7 @@ class QuotaholderTest(TestCase):
         r = qh.get_pending_commissions(clientkey=self.client)
         self.assertEqual(len(r), 1)
         serial = r[0]
+        self.assertEqual(serial, s1)
         r = qh.resolve_pending_commission(self.client, serial)
         self.assertEqual(r, True)
         r = qh.get_pending_commissions(clientkey=self.client)
@@ -150,6 +151,13 @@ class QuotaholderTest(TestCase):
                   (holder, source, resource2): (limit2, limit2, limit2),
                   }
         self.assertEqual(r, quotas)
+
+        logs = models.ProvisionLog.objects.filter(serial=serial)
+        self.assertEqual(len(logs), 2)
+        log1 = filter(lambda p: p.resource == resource1
+                      and p.delta_quantity == limit1/2
+                      and p.usage_min == limit1/2, logs)
+        self.assertEqual(len(log1), 1)
 
         # resolve commissions
 
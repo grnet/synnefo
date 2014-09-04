f4a7ef792490e65c5ec4fb01a58fedf2fe233510
Alex Pyrgiotis <apyrgio@grnet.gr>
2014-09-04 17:48:46 +0300

    admin: Migrate from Highcharts to C3
    For every chart that uses Highcharts, add its C3 counterpart below it.
    The change from Highcharts to C3 is for license reasons.

--- snf-admin-app/synnefo_admin/admin/static/js/charts_common.js
+++ snf-admin-app/synnefo_admin/admin/static/js/charts_common.js
@@ -11,15 +11,15 @@ $(document).ready(function() {
         success: function(response, statusText, jqXHR) {
             astakos_stats = response;
             infraUsage(astakos_stats);
+            for (var key in astakos_stats.resources.all) {
+                resourceUsage(astakos_stats, key);
+            }
             statusPerProvider(astakos_stats);
             statusPerProviderReversed(astakos_stats);
             exclusiveProviders(astakos_stats);
-            for (var key in astakos_stats.resources.all) {
-	            resourceUsage(astakos_stats, key);
-            };
         },
         error: function(jqXHR, statusText) {
-            console.log('error', statusText)
+            console.log('error', statusText);
         }
     });
 
@@ -28,25 +28,24 @@ $(document).ready(function() {
         type: 'GET',
         contentType: 'application/json',
         success: function(response, statusText, jqXHR) {
-            cyclades_stats = response
+            cyclades_stats = response;
             serverStatus(cyclades_stats);
             ipPoolStatus(cyclades_stats);
             diskTemplates(cyclades_stats);
             imagesStats(cyclades_stats);
         },
         error: function(jqXHR, statusText) {
-            console.log('error', statusText)
+            console.log('error', statusText);
         }
     });
 
-   
     // when page is loaded show charts whose sidebar a is active
     function display_charts_init(){
         $('.charts .sidebar a.active').each(function(){
             var el = $(this).attr('data-chart');
             $('.well').find('div[data-chart='+el+']').show();
         });
-    };
+    }
 
     display_charts_init();
 
@@ -54,6 +53,6 @@ $(document).ready(function() {
         e.preventDefault();
         $(this).toggleClass('active');
         var el = $(this).attr('data-chart');
-        $('.well').find('div[data-chart='+el+']').stop(true, false).slideToggle('slow')
+        $('.well').find('div[data-chart='+el+']').stop(true, false).slideToggle('slow');
     });
 });

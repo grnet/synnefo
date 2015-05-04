40265ceafc215e90d1d8e2fa26560cc63c23872e
Stratos Psomadakis <psomas@grnet.gr>
2015-05-04 15:17:05 +0300

    astakos: Fix UI project tests

--- snf-astakos-app/astakos/im/tests/projects.py
+++ snf-astakos-app/astakos/im/tests/projects.py
@@ -796,9 +796,11 @@ class TestProjects(TestCase):
             'member_join_policy': 2,
             'member_leave_policy': 1,
             'limit_on_members_number_0': 5,
+            'service1.resource_p_uplimit': 100,
             'service1.resource_m_uplimit': 100,
             'is_selected_service1.resource': "1",
-            'astakos.pending_app_m_uplimit': 100,
+            'astakos.pending_app_m_uplimit': 10,
+            'astakos.pending_app_p_uplimit': 100,
             'is_selected_accounts': "1",
             'user': self.user.pk
         }
@@ -807,6 +809,7 @@ class TestProjects(TestCase):
         self.assertEqual(form.is_valid(), False)
 
         del application_data['astakos.pending_app_m_uplimit']
+        del application_data['astakos.pending_app_p_uplimit']
         del application_data['is_selected_accounts']
         form = forms.ProjectApplicationForm(data=application_data)
         self.assertEqual(form.is_valid(), True)

cc1423b876676d9b17c21b7c715cef2ddef49211
Vitalis Salis <vitsalis@grnet.gr>
2016-01-14 18:18:15 +0200

    Add `get_public_stats_from_cache` function
    Until now the function that returned the public statistics
    for okeanos did a costly query to the database. In order
    to avoid that the `get_public_stats_from_cache` was created
    that retrieves the public statistics from the cache. The old
    function wasn't removed for backwards compatibility reasons.

--- snf-cyclades-app/synnefo/api/tests/__init__.py
+++ snf-cyclades-app/synnefo/api/tests/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2014 GRNET S.A.
+# Copyright (C) 2010-2016 GRNET S.A.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,3 +24,4 @@ from .extensions import *
 from .subnets import *
 from .floating_ips import *
 from .util import *
+from .admin_stats import *

4e90428cd4ea9c63da73bf0c80e0a9ac6f119bc6
Giorgos Korfiatis <gkorf@grnet.gr>
2016-04-05 12:52:22 +0300

    astakos: Use user_logic in user api
    Eliminate use of activation_backends from the user api.

--- snf-astakos-app/astakos/api/user.py
+++ snf-astakos-app/astakos/api/user.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2014 GRNET S.A.
+# Copyright (C) 2010-2016 GRNET S.A.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -37,11 +37,9 @@ from astakos.im import settings
 from astakos.admin import stats
 from astakos.im.models import AstakosUser, get_latest_terms
 from astakos.im.auth import make_local_user
-from astakos.im import activation_backends
+from astakos.im import user_logic
 
 ADMIN_GROUPS = settings.ADMIN_API_PERMITTED_GROUPS
-
-activation_backend = activation_backends.get_backend()
 
 logger = logging.getLogger(__name__)
 
@@ -202,14 +200,14 @@ def users_create(request):
 
         user = AstakosUser.objects.get(pk=user.pk)
         code = user.verification_code
-        ver_res = activation_backend.handle_verification(user, code)
+        ver_res = user_logic.verify(user, code)
         if ver_res.is_error():
             raise Exception(ver_res.message)
 
         # in case of auto moderation user moderation is handled within the
         # verification process, no need to reapply moderation process
         if not user.moderated:
-            mod_res = activation_backend.handle_moderation(user, accept=True)
+            mod_res = user_logic.accept(user, accept=True, notify_user=False)
             if mod_res.is_error():
                 raise Exception(ver_res.message)
 
@@ -237,7 +235,7 @@ def user_action(request, user_id):
         except AstakosUser.DoesNotExist:
             raise faults.ItemNotFound("User not found")
 
-        activation_backend.activate_user(user)
+        user_logic.activate(user)
 
         user = AstakosUser.objects.get(uuid=user_id)
         user_data = {
@@ -253,7 +251,7 @@ def user_action(request, user_id):
         except AstakosUser.DoesNotExist:
             raise faults.ItemNotFound("User not found")
 
-        activation_backend.deactivate_user(
+        user_logic.deactivate(
             user, reason=req['deactivate'].get('reason', None))
 
         user = AstakosUser.objects.get(uuid=user_id)

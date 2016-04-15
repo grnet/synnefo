e46a0207fac8ac3cbdf37f8e16d9b80ea2582b9a
Yiannis Tsiouris <tsiou@grnet.gr>
2016-04-15 17:03:44 +0300

    Change the dispatcher's logging severity to "INFO"
    This used to be "DEBUG" but maybe all that information was only useful to
    developers.

--- snf-cyclades-app/synnefo/cyclades_settings.py
+++ snf-cyclades-app/synnefo/cyclades_settings.py
@@ -49,11 +49,9 @@ VOLUME_PREFIX = cyclades_services['cyclades_volume']['prefix']
 
 COMPUTE_ROOT_URL = join_urls(BASE_URL, COMPUTE_PREFIX)
 
-DISPATCHER_LOGGING_LEVEL = getattr(settings, 'DISPATCHER_LOGGING_LEVEL',
-                                   logging.DEBUG)
-AMQP_LOGGING_LEVEL = getattr(settings, 'AMQP_LOGGING_LEVEL', logging.DEBUG)
-LOGIC_LOGGING_LEVEL = getattr(settings, 'SYNNEFO_LOGIC_LOGGING_LEVEL',
-                              logging.DEBUG)
+DISPATCHER_LOGGING_LEVEL = getattr(settings, 'DISPATCHER_LOGGING_LEVEL', 'INFO')
+AMQP_LOGGING_LEVEL = getattr(settings, 'AMQP_LOGGING_LEVEL', 'INFO')
+LOGIC_LOGGING_LEVEL = getattr(settings, 'LOGIC_LOGGING_LEVEL', 'INFO')
 
 
 # --------------------------------------------------------------------

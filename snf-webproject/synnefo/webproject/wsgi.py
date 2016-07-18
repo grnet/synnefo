fd1f9f570d4cde660d1375c84ddfb92c5a7d42ba
Filippos Giannakos <philipgian@grnet.gr>
2016-07-18 11:02:13 +0300

    webproject: Load the app in gunicorn in wsgi mode
    Create a wsgi application as recommended by django since 1.4.
    https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/gunicorn/
    
    Load this module in gunicorn using its 'wsgi' mode. Mode 'django'
    previously used is deprecated since gunicorn 18.0.
    http://docs.gunicorn.org/en/18.0/news.html#id1

---
+++ snf-webproject/synnefo/webproject/wsgi.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2010-2016 GRNET S.A.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "synnefo.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()

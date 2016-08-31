bf8ae0703d16531787362f1d04c4b9ba71e9fae0
Nikos Skalkotos <skalkoto@grnet.gr>
2016-08-31 14:08:20 +0300

    docs: Update copyright date

--- snf-webproject/docs/conf.py
+++ snf-webproject/docs/conf.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath('../'))
 from synnefo.versions.webproject import __version__
 
 project = u'snf-webproject'
-copyright = u'2012-2013, GRNET'
+copyright = u'2012-2016, GRNET'
 version = __version__
 release = __version__
 html_title = 'synnefo ' + version

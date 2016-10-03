edc0caa1c0398f18b47349329c246fad645b7150
Filippos Giannakos <philipgian@grnet.gr>
2016-10-03 16:25:35 +0300

    webproject: Decode isoformatted strings to datetime
    Since the Session Serializer serializes dates to isoformatted strings,
    when we decode the object, we should convert those strings back to
    datetime objects for the framework to use. We can unconditionally
    convert all matching strings to date, since we control the session data
    and there should be no other matching string that will be wrongfully
    converted to datetime object.

--- snf-webproject/synnefo/webproject/serializer.py
+++ snf-webproject/synnefo/webproject/serializer.py
@@ -1,5 +1,27 @@
-from django.core.serializers.json import DjangoJSONEncoder
 import json
+import logging
+from dateutil import parser
+from django.core.serializers.json import DjangoJSONEncoder
+
+logger = logging.getLogger(__name__)
+
+
+class DateTimeDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook,
+                                  *args, **kwargs)
+
+    def object_hook(self, obj):
+        for (key, value) in obj.items():
+            try:
+                # Convert only top level strings, since there are no nested
+                # dates in the Session object. In such a case, this must be
+                # extended to recursivelly convert isoformatted strings to
+                # datetime objects.
+                obj[key] = parser.parse(value)
+            except Exception:
+                pass
+        return obj
 
 
 class DateTimeAwareJSON(object):
@@ -7,4 +29,8 @@ class DateTimeAwareJSON(object):
         return json.dumps(obj, cls=DjangoJSONEncoder)
 
     def loads(self, data):
-        return json.loads(data)
+        try:
+            return json.loads(data, cls=DateTimeDecoder)
+        except Exception as e:
+            logger.error("Failed to load session: " + str(e))
+            raise

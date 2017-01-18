761423c81ae83cf36c749e2fecee22670c12266d
Nikos Skalkotos <skalkoto@grnet.gr>
2017-01-18 12:36:12 +0200

    Update Changelog and version.py files for 0.19rc1

--- snf-cyclades-gtools/synnefo/versions/ganeti.py
+++ snf-cyclades-gtools/synnefo/versions/ganeti.py
@@ -1,3 +1,8 @@
 
-__version__ = "0.19.dev0"
-
+__version__ = "0.19rc1"
+__version_vcs_info__ = {
+    'branch': 'release-0.19',
+    'revid': '6183a32',
+    'revno': 12967}
+__version_user_email__ = "skalkoto@grnet.gr"
+__version_user_name__ = "Nikos Skalkotos"

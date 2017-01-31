c57fe012b6247908e9241bf4ad18e66b492e0c4f
Filippos Giannakos <philipgian@grnet.gr>
2017-01-31 12:34:33 +0200

    cyclades: Update flavor API wrt flavor-access
    Update the flavor API to return information regarding flavor-access, and
    to filter flavor listing and flavor details based on only accessible
    flavors. A flavor is accessible if it is public or a project membership
    grants access.
    
    More specifically:
    
     * Enrich the flavor's details with the Openstack compatible
       'os-flavor-access:is_public' attribute and the proprietary
       'SNF:flavor-access' which holds which projects of the user have access
       to the flavor.
     * Limit flavor listing based on the public attribute and the projects
       granting access. If both filters are used simultaneously, flavors
       satisfying both are returned.

--- snf-cyclades-app/synnefo/api/flavors.py
+++ snf-cyclades-app/synnefo/api/flavors.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2016 GRNET S.A.
+# Copyright (C) 2010-2017 GRNET S.A.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,6 +18,7 @@ from django.conf.urls import patterns
 
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.db.models import Q
 import json
 
 from snf_django.lib import api
@@ -36,16 +37,19 @@ urlpatterns = patterns(
 )
 
 
-def flavor_to_dict(flavor, detail=True):
+def flavor_to_dict(flavor, detail=True, projects=[]):
     d = {'id': flavor.id, 'name': flavor.name}
     d['links'] = util.flavor_to_links(flavor.id)
     if detail:
         d['ram'] = flavor.ram
         d['disk'] = flavor.disk
         d['vcpus'] = flavor.cpu
+        d['os-flavor-access:is_public'] = flavor.public
         d['SNF:disk_template'] = flavor.volume_type.disk_template
         d['SNF:volume_type'] = flavor.volume_type_id
         d['SNF:allow_create'] = flavor.allow_create
+        d['SNF:flavor-access'] = [a.project for a in flavor.access.all()
+                                  if a.project in projects]
     return d
 
 
@@ -59,9 +63,38 @@ def list_flavors(request, detail=False):
     #                       overLimit (413)
 
     log.debug('list_flavors detail=%s', detail)
+
+    public = request.GET.get('is_public')
+    project = request.GET.get('SNF:flavor-access')
+
     active_flavors = Flavor.objects.select_related("volume_type")\
                                    .exclude(deleted=True)
-    flavors = [flavor_to_dict(flavor, detail)
+    if detail:
+        active_flavors = active_flavors.prefetch_related("access")
+
+    # This way of building the queryset, results in relative complex query,
+    # having unnecessary 'WHERE' clauses but results on a much cleaner code.
+    # The database should be able to efficiently evaluate the produced SQL
+    # query.
+    #
+    # That's the maximum you can get, if you set no filters
+    active_flavors = active_flavors.filter(
+        Q(access__project__in=request.user_projects) |
+        Q(public=True))
+
+    if project is not None:  # restricting by project
+        # if filtering by a project that the user does not belong to,
+        # then no flavors must be returned because the user does not
+        # have access to any flavor of this project
+        if project not in request.user_projects:
+            project = ''
+        active_flavors = active_flavors.filter(access__project=project)
+
+    if public is not None:  # restricting by public flag
+        public = public.lower() == 'true'
+        active_flavors = active_flavors.filter(public=public)
+
+    flavors = [flavor_to_dict(flavor, detail, request.user_projects)
                for flavor in active_flavors.order_by('id')]
 
     if request.serialization == 'xml':
@@ -85,8 +118,10 @@ def get_flavor_details(request, flavor_id):
     #                       overLimit (413)
 
     log.debug('get_flavor_details %s', flavor_id)
-    flavor = util.get_flavor(flavor_id, include_deleted=True)
-    flavordict = flavor_to_dict(flavor, detail=True)
+    flavor = util.get_flavor(flavor_id, include_deleted=True,
+                             for_project=request.user_projects)
+    flavordict = flavor_to_dict(flavor, detail=True,
+                                projects=request.user_projects)
 
     if request.serialization == 'xml':
         data = render_to_string('flavor.xml', {'flavor': flavordict})
